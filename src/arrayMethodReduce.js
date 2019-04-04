d93780836d9504fa838caaf7857cc7beda7b0cc5
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 16:06:49 +0300

    fixed eslint errors

---
+++ src/arrayMethodReduce.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method Reduce
+ */
+function applyCustomReduce() {
+  [].__proto__.reduce2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomReduce;
