b64e21402e6e3e5ef81404b3c802b61efd41e219
Olegus <gulldan1996@gmail.com>
2019-04-11 19:53:52 +0300

    reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,21 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function() {
-    // write code here
+  [].__proto__.reduce2 = function(callback, startValue) {
+    const items = this;
+    let prevValue = startValue;
+    let startIndex = 0;
+
+    if (startValue === undefined) {
+      prevValue = items[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < items.length; i++) {
+      let item = items[i];
+      prevValue = callback(prevValue, item, i, items);
+    }
+    return prevValue;
   };
 }
 
