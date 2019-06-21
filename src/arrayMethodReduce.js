cbc8af9745eb5f39aa41903946cab954687f11c8
OleksandrHlybchenko <logista@meta.ua>
2019-06-21 23:09:28 +0300

    :ok_hand:done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,20 +4,14 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    let accum;
-    let i = 0;
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
+    let i = (arguments.length < 2) ? 1 : 0;
+    let accum = initialValue;
 
-    if (initialValue !== undefined) {
-      accum = initialValue;
-    } else {
-      accum = this[0];
-      i = 1;
-    }
-
-    for (i; i < this.length; i++) {
+    while (i < this.length) {
       accum = callback(accum, this[i], i, this);
-    };
+      i++;
+    }
 
     return accum;
   };
