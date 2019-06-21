f36cb956294e769217a8558bd3de8b6bf16c8a84
Пользователь <Sviatoslv>
2019-06-21 22:52:26 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,35 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this.length < 1) {
+      return initialValue;
+    }
+
+    let accumulator = 0;
+    let previousValue = this[0];
+
+    if (initialValue !== undefined) {
+      previousValue = initialValue;
+      for (let i = 0; i < this.length; i++) {
+        const item = this[i];
+        const index = i;
+        const arr = this;
+        accumulator = callback(previousValue, item, index, arr);
+        previousValue = accumulator;
+      }
+
+      return accumulator;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      const item = this[i];
+      const index = i;
+      const arr = this;
+      accumulator = callback(previousValue, item, index, arr);
+      previousValue = accumulator;
+    }
+
+    return accumulator;
   };
 }
 
