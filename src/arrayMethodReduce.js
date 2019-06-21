2638957b0885bb19cdb1bf34336e5f167c04cf11
Alc499 <Ali.vitchenko@gmail.com>
2019-06-21 13:47:21 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,26 +4,29 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
-    if (this.length < 1 && initialValue === this[0]) {
-      return TypeError;
+  [].__proto__.reduce2 = function(callback, initialValue) {
+    if (this.length < 1 && !initialValue) {
+      throw new TypeError('Reduce of empty array with no initial value');
     }
 
-    if ((this.length === 1 && initialValue === this[0])
-      || (this.length < 1 && initialValue !== this[0])) {
+    if ((this.length === 1 && !initialValue)
+      || this.length < 1) {
       return initialValue;
     }
 
-    let result = initialValue;
+    let result;
+    let i;
 
-    if (initialValue === this[0]) {
-      for (let i = 1; i < this.length; i++) {
-        result = callback(result, this[i], i, this);
-      }
+    if (initialValue !== undefined) {
+      result = initialValue;
+      i = 0;
     } else {
-      for (let i = 0; i < this.length; i++) {
-        result = callback(result, this[i], i, this);
-      }
+      result = this[0];
+      i = 1;
+    }
+
+    for (; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
     }
 
     return result;
