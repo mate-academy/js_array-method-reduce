572f8bcd7b6090719161c073f86dab8fc11f7ea9
RavenVol <r.volianiuk@gmail.com>
2019-06-22 03:41:04 +0300

    Reduce2 - Solved by Raven

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,21 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let accumulator;
+
+    if (this.length <= 0) {
+      return initialValue;
+    }
+
+    initialValue !== undefined
+      ? accumulator = callback(initialValue, this[0], 0, this)
+      : accumulator = this[0];
+
+    for (let i = 1; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
