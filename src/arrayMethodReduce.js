e61c9ed296844c33dc0567ebed17b35a909d6a73
bratyslav <beginner.altavista@gmail.com>
2019-06-24 13:18:45 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,20 +4,9 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator = 0;
-    let currentValueIndex = 0;
-
-    if (initialValue === undefined) {
-      if (this.length === 0) {
-        throw new TypeError('Empty array and no initial value');
-      } else {
-        accumulator = this[0];
-        currentValueIndex = 1;
-      }
-    } else {
-      accumulator = initialValue;
-    }
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
+    let accumulator = initialValue;
+    const currentValueIndex = (initialValue === this[0] ? 1 : 0);
 
     for (let i = currentValueIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
