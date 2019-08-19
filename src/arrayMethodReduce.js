915360891c2e46ec9c4ddbc4747ff0c7be5eb1d6
ivan_safiulin <ivansafiulin@myrambler.ru>
2019-08-20 00:33:17 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,9 +3,22 @@
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accValue;
+
+    if (initialValue === undefined) {
+      accValue = (typeof this[0] === 'string') ? '' : 0;
+    } else {
+      accValue = initialValue;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      accValue = callback(accValue, this[i], i, this);
+    }
+
+    return accValue;
   };
 }
 
