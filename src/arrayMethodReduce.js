d17fcb0bf56cb5ca6d1bb6db32288345195ab537
Taisiia <taisiia.pomazan@gmail.com>
2019-08-22 20:27:46 +0300

    sol

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let i = 0;
+    let resultValue;
+    arguments.length <= 1 ? i = 1 : i = 0;
+    arguments.length <= 1 ? resultValue = this[0] : resultValue = initialValue;
+
+    for (; i < this.length; i++) {
+      resultValue = callback(resultValue, this[i], i, this);
+    };
+
+    return resultValue;
   };
 }
 
