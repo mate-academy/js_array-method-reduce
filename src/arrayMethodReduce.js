55fa30e16b4c07199b95640e6f6978cca0723cd7
Dmytro <voronok16@gmail.com>
2019-08-27 12:29:54 +0300

    fix variable name

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,14 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
-    let redusedValue = initialValue;
+    let reducedValue = initialValue;
+
     if (typeof initialValue === 'string') {
-      redusedValue = '';
+      reducedValue = '';
     }
     for (let i = 0; i < this.length; i++) {
-      redusedValue = callback(redusedValue, this[i], i, this);
+      reducedValue = callback(reducedValue, this[i], i, this);
     }
-    return redusedValue;
+
+    return reducedValue;
   };
 }
 
