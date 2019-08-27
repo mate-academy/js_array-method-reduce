aa8b5f168a95355e929ada1f9a4fd9c6d393670a
Aleks <alex.andrutskii@gmail.com>
2019-08-27 20:50:55 +0300

    Naming changes

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let acc = this[0];
+    let accumulator = this[0];
     let count = 1;
 
     if (arguments[1] !== undefined) {
-      acc = initialValue;
+      accumulator = initialValue;
       count = 0;
     }
 
     for (count; count < this.length; count++) {
-      acc = callback(acc, this[count], count, this);
+      accumulator = callback(accumulator, this[count], count, this);
     }
 
-    return acc;
+    return accumulator;
   };
 }
 
