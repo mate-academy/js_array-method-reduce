af0e375c892cbc6be3041c33a6d7056234d7f8e5
Taisiia <taisiia.pomazan@gmail.com>
2019-08-27 17:43:07 +0300

    sol

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,9 +5,9 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let i = 0;
+    let i = arguments.length <= 1 ? 1 : 0;
     let resultValue = arguments.length <= 1
-      ? (i = 1) && this[0]
+      ? this[0]
       : initialValue;
 
     for (; i < this.length; i++) {
