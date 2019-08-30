123cd32fb7df62f233ca253c2731f7a6f64dfc94
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-30 12:34:36 +0300

    refactoring

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -7,12 +7,14 @@ function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     let accumulator;
     let i = 0;
+
     if (initialValue === undefined) {
       accumulator = this[i];
       i++;
     } else {
       accumulator = initialValue;
     }
+
     for (i; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
     }
