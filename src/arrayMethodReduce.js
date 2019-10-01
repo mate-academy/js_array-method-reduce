362212588ee7067620daf1d694f64cd6c8dd7509
Serhii <gespenst.serge@yandex.ru>
2019-10-01 22:39:04 +0300

    sol

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulattor = arguments.length === 1 ? this[0] : initialValue;
+    let i = arguments.length === 1 ? 1 : 0;
+
+    for (i; i < this.length; i++) {
+      accumulattor = callback(accumulattor, this[i], i, this);
+    }
+
+    return accumulattor;
   };
 }
 
