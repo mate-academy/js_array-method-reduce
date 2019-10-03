490b9fa67e77ded6289a86cd021fbd336283cb2e
VadimK <djvadya16@gmail.com>
2019-10-03 16:46:41 +0300

    Kisilov-Vadim

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,9 +3,17 @@
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let rezult = initialValue === undefined ? this[0] : initialValue;
+    let i = initialValue === undefined ? 1 : 0;
+
+    for (; i < this.length; i++) {
+      rezult = callback(rezult, this[i], i, this);
+    }
+
+    return rezult;
   };
 }
 
