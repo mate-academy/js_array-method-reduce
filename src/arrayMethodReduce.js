689d941b431237363969ed0b2c761f466c2836e1
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-11-20 21:43:39 +0200

    Aidinov Alexii

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,34 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this.length === 0) {
+      return initialValue;
+    }
+
+    let result = 0;
+    let accumulator = 0;
+
+    if (initialValue !== undefined) {
+      accumulator = initialValue;
+      if (typeof this[0] === 'string') {
+        result = '';
+      }
+    } else {
+      if (typeof this[0] === 'string') {
+        result = '';
+        accumulator = '';
+      }
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof accumulator === 'number') {
+        result = +result;
+      }
+
+      result += callback(accumulator, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
