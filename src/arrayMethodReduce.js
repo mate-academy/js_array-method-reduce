1bbcd17307f9bb623fcacf709bad2cdf39e062fd
Olexander <kravchenko.sania@gmail.com>
2020-01-27 14:40:46 +0200

    solution reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,26 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (!this.length) {
+      if (initialValue !== undefined) {
+        return initialValue;
+      } else {
+        throw new TypeError();
+      }
+    }
+
+    if (this.length === 1 && !initialValue) {
+      return this[0];
+    }
+
+    let startValue = initialValue !== undefined ? initialValue : this[0];
+    let startIndex = initialValue !== undefined ? 0 : 1;
+
+    for (; startIndex < this.length; ++startIndex) {
+      startValue = callback(startValue, this[startIndex], startIndex, this);
+    }
+
+    return startValue;
   };
 }
 
