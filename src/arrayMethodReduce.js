12c90cd706cfa2ab61902554609c3c45481682f8
Oleksii Mykhailov <alem.coding@gmail.com>
2020-04-03 19:45:32 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum = this[0];
+    let startIndex = 1;
+
+    if (initialValue !== undefined) {
+      sum = initialValue;
+      startIndex = 0;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum;
   };
 }
 
