bfadc2323b50e404b40746e9f5f24732d5166f67
Kostya Peniyavskiy <kornelius9898@gmail.com>
2020-04-05 15:38:09 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,25 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator = 0;
+
+    if (typeof initialValue === 'number') {
+      accumulator = initialValue;
+    }
+
+    if (typeof this[0] === 'string') {
+      accumulator = '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+
+      if (isNaN(+accumulator) !== true) {
+        accumulator = +accumulator;
+      }
+    }
+
+    return accumulator;
   };
 }
 
