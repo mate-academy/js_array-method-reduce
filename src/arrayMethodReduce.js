3e5548d29638c9360a7fe0a59806f9c61da034fe
Yuriy Panasiuk <yur.panasiuk@gmail.com>
2020-04-06 16:02:54 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,21 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let acum = 0;
+
+    if (initialValue !== undefined) {
+      acum = initialValue;
+    } else {
+      acum = this[0];
+    }
+
+    let i = initialValue !== undefined ? 0 : 1;
+
+    for (; i < this.length; i++) {
+      acum = callback(acum, this[i], i, this);
+    };
+
+    return acum;
   };
 }
 
