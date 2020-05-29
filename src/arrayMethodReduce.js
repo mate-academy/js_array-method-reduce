b6ec178a6541014b3e35e26bcaa018dd45704375
solaryasha <solaryasha@gmail.com>
2020-05-29 15:05:45 +0300

    My solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,23 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
-  };
-}
+    let accumulator = initialValue;
 
+    for (let i = 0; i < this.length; i++) {
+      if (initialValue === undefined && i === 0) {
+        accumulator = this[i];
+      } else {
+        accumulator = callback(accumulator, this[i], i, this);
+      }
+    }
+
+    return accumulator;
+  };
+};
+
+// const sourceNumbers = [0, 10, 20, 30];
+// const sourceStr = ['a', 'b', 'c', 'd'];
+
+applyCustomReduce();
+// sourceStr.reduce2((sum, x) => sum + x);
 module.exports = applyCustomReduce;
