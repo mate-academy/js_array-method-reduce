62d19268d3dde44f64518396f15887ced8ea5d39
tyooma <flying.alien.godzilla@gmail.com>
2020-06-01 23:31:21 +0300

    REDUCE METHOD

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,26 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const arr = Object(this);
+    let accumulator;
+    let i = 0;
+
+    if (arguments.length >= 2) {
+      accumulator = arguments[1];
+    } else {
+      while (i < arr.length && !(i in arr)) {
+        i++;
+      }
+      accumulator = arr[i++];
+    }
+
+    for (; i < this.length; i++) {
+      if (i in arr) {
+        accumulator = callback(accumulator, this[i], i, this);
+      }
+    }
+
+    return accumulator;
   };
 }
 
