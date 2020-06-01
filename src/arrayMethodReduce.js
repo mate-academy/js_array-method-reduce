e309745b708e2005394382bff084b9555d8f32eb
Denys <gryshko_denys@gmail.com>
2020-06-01 16:56:08 +0300

    fixed first iteration

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,7 +6,7 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     let accumulator = initialValue;
-    let i = initialValue ? 1 : 0; // check for for first iteration
+    let i = initialValue === this[0] ? 1 : 0; // check for for first iteration
 
     for (i; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
