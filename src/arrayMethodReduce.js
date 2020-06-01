5e3742642e4b13613da0db1db8eeca467173cf31
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-06-01 12:51:57 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let res;
+
+    (isNaN(this[0])) ? res = initialValue : res = 0;
+
+    if (this[0] === this[0] + '' && initialValue === undefined) {
+      res = '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      res = callback(res, this[i], i, this);
+    }
+
+    return res;
   };
 }
 
