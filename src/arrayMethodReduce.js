349211b35eeeea6a19c268293f6771d0a576cd79
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-01 18:46:36 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -6,6 +7,25 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let accumReturns = initialValue;
+    let start = 0;
+    let startVal = initialValue;
+
+    if (initialValue === undefined) {
+      if (typeof this[0] === 'string') {
+        startVal = '';
+      } else {
+        startVal = 0;
+      }
+      accumReturns = this[0];
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      accumReturns += callback(startVal, this[i], i, this);
+    }
+
+    return accumReturns;
   };
 }
 
