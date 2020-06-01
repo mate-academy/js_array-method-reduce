7098a9a0d1421f603e349f06d51ddded6263d5f4
Denys <gryshko_denys@gmail.com>
2020-06-01 15:15:42 +0300

    changed result to accumulator

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,14 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
-    let result = initialValue;
+    let accumulator = initialValue;
     let i = initialValue ? 1 : 0; // check for for first iteration
 
     for (i; i < this.length; i++) {
-      result = callback(result, this[i], i, this);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
-    return result;
+    return accumulator;
   };
 }
 
