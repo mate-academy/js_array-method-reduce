55a25823dbcd6a9744b486872d5da626e3284daf
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-02 19:15:01 +0300

    refactor code

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -8,20 +8,14 @@ function applyCustomReduce() {
     // write code here
     let accumulator = initialValue;
     let start = 0;
-    let startVal = initialValue;
 
     if (initialValue === undefined) {
-      if (typeof this[0] === 'string') {
-        startVal = '';
-      } else {
-        startVal = 0;
-      }
       accumulator = this[0];
       start = 1;
     }
 
     for (let i = start; i < this.length; i++) {
-      accumulator += callback(startVal, this[i], i, this);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
     return accumulator;
