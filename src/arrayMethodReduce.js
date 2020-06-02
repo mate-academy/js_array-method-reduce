2082e6f57ab1558e2eb08bd6b5727884fc97cdeb
Vovasef <vovasef@gmail.com>
2020-06-02 13:31:58 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -14,7 +14,7 @@ function applyCustomReduce() {
     }
 
     for (let i = 0; i < this.length; i++) {
-      accumulator = callback(accumulator, this[i], i, this, initialValue);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
     return accumulator;
