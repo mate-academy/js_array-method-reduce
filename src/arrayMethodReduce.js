fa92f6019ad5d81e393a51435b9a7f6c70dd4e83
ArtemiiKab <artemiikab@gmail.com>
2020-08-10 12:35:14 +0300

    reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,18 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let acc = initialValue;
+
+    for (let i = 0; i < this.length; i++) {
+      if (initialValue === undefined && i === 0) {
+        i++;
+        acc = callback(this[0], this[i], i, this);
+      } else {
+        acc = callback(acc, this[i], i, this);
+      }
+    }
+
+    return acc;
   };
 }
 
