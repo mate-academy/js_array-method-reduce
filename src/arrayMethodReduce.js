2e8cc33a2fe5d6be5a968678de51295d1bc3c4a4
Alisa <aliceblunt8@gmail.com>
2020-08-11 13:46:37 +0300

    fix

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,22 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let returnValue = initialValue;
-    let startIndex = 0;
-
-    if (arguments.length === 0) {
+    if (arguments.length === 0 || typeof callback !== 'function') {
       return;
     }
 
-    if (this.length === 1 && initialValue === undefined) {
-      return this[0];
-    }
+    let returnValue = initialValue;
+    let startIndex = 0;
 
-    if (this.length === 0 && initialValue) {
-      return initialValue;
-    }
-
-    if (arguments.length === 1) {
+    if (initialValue === undefined) {
       startIndex = 1;
       returnValue = this[0];
     }
