3e0d3a7750edc67d9ba2d6f935d8ba9877361ae0
artsashko <artyomsashko@gmail.com>
2020-08-12 00:12:56 +0300

    Fixes

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,12 +6,13 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     let result = initialValue;
+    let startIndex = 0;
 
-    if (typeof result === 'string') {
-      result = '';
+    if (arguments.length < 2) {
+      startIndex = 1;
     }
 
-    for (let i = 0; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       result = callback(result, this[i], i, this);
     }
 
