93616fb7f854eb47d03439fca71141461d6eb908
oggyrock <steelo.sun@gmail.com>
2020-08-11 14:10:19 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,13 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accum = initialValue || 0;
+
+    for (let i = 0; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return typeof accum === 'string' ? accum.substr(1) : accum;
   };
 }
 
