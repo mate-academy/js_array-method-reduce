0013dbd6bdc3a81657ae8022c8c90f13adab74c5
Elena-brv <leena.brv@gmail.com>
2020-08-12 16:32:45 +0300

    updated

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,18 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
-    const result = [];
+    let prev = initialValue;
+    let i = 0;
 
-    if (initialValue !== this[0]) {
-      result.push(callback(initialValue, this[0], 0, this));
-    } else {
-      result.push(this[0]);
+    if (prev === this[0]) {
+      i = 1;
     }
 
-    for (let i = 1; i < this.length; i++) {
-      result.push(callback(result[i - 1], this[i], i, this));
+    for (; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
     }
 
-    return result[result.length - 1];
+    return prev;
   };
 };
 
