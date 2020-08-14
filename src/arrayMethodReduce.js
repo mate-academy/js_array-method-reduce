39cb2e92953a82f748c29cb33a8765a0f71e8575
Zarva <zarva6596@gmail.com>
2020-08-14 12:56:19 +0300

    fixed erron with different types

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,17 +4,19 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue = '') {
+  [].__proto__.reduce2 = function(callback, initialValue) {
     let start = initialValue;
+    let i = 0;
 
-    if (typeof this[0] === 'number') {
-      start = 0;
+    if (start === undefined) {
+      start = this[0];
+      i = 1;
     }
 
     let result = start;
 
-    for (let i = 0; i < this.length; i++) {
-      result += callback(start, this[i], i, this);
+    for (i; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
     }
 
     return result;
