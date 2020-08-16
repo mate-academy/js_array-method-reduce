3b296472a5c3ee1bb72d749e7a4274505801de57
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-17 00:43:16 +0300

    done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,22 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, initialValue = 0) {
+    let res = initialValue;
+
+    if (typeof this[0] === 'string') {
+      res = '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      res = callback(res, this[i], i, this);
+
+      if (!isNaN(parseInt(res))) {
+        res = parseInt(res);
+      }
+    }
+
+    return res;
   };
 }
 
