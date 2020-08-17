338775756ee126e818255c1f13d57cf2064638d8
ToadyOne <dimonua@gmail.com>
2020-08-17 18:33:22 +0300

    solved

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,27 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (!initialValue) {
+      let accum;
+
+      typeof callback(accum, this[0], 0, this) === 'number'
+        ? accum = 0
+        : accum = '';
+
+      for (let i = 0; i < this.length; i++) {
+        accum = callback(accum, this[i], i, this);
+      }
+
+      return accum;
+    } else {
+      let accum = initialValue;
+
+      for (let i = 1; i < this.length; i++) {
+        accum = callback(accum, this[i], i, this);
+      }
+
+      return accum;
+    }
   };
 }
 
