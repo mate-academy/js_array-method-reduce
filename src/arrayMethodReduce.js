72d29e034fa470930df2b1da58fa321ec9c5934d
Alona <aionovaionova@gmail.com>
2020-09-28 09:43:27 +0300

    done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,27 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result;
+
+    if (initialValue !== undefined) {
+      result = initialValue;
+    } else {
+      result = this[0];
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (initialValue === undefined) {
+        if (this[i + 1] !== undefined) {
+          result = callback(result, this[i + 1], i + 1, this);
+        }
+      } else {
+        if (this[i] !== undefined) {
+          result = callback(result, this[i], i, this);
+        }
+      }
+    }
+
+    return result;
   };
 }
 
