aea327b8bcaa298283405bebf707cea4fd67fe14
Tania Omelko <tanyaomelko39@gmail.com>
2020-10-05 13:57:53 +0300

    fix

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,12 +6,14 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     let endValue = initialValue;
+    let start = 0;
 
-    if (endValue === undefined) {
-      endValue = typeof (this[0]) === 'string' ? '' : 0;
+    if (initialValue === undefined) {
+      endValue = this[0];
+      start = 1;
     }
 
-    for (let i = 0; i < this.length; i++) {
+    for (let i = start; i < this.length; i++) {
       endValue = callback(endValue, this[i], i, this);
     }
 
