6818bdc96fe3ea96b4dc64f2667c3225c3d150c4
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-10-06 02:06:52 +0300

    i variable inserted in loop

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,13 +6,8 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     let accumulatedValue = initialValue;
-    let i = 0;
 
-    if (arguments.length === 1) {
-      i = 1;
-    }
-
-    for (; i < this.length; i++) {
+    for (let i = arguments.length === 1 ? 1 : 0; i < this.length; i++) {
       accumulatedValue = callback(accumulatedValue, this[i], i, this);
     }
 
