736aa1b8f47d58946ebd9e0aceee0a7010ac8f87
AndrewKreshchenko <mail2andyk@gmail.com>
2020-12-02 15:03:49 +0200

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,28 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this === null) {
+      throw new TypeError('reduce2 called on null or undefined.'
+        + 'Method reduce2 must be called on an array object.');
+    }
+
+    if (typeof callback !== 'function') {
+      throw new TypeError(callback + ' is not a function');
+    }
+
+    let firstValue = initialValue || (isNaN(this[0]) ? '' : 0);
+
+    if (initialValue === 0) {
+      firstValue = 0;
+    }
+
+    let acumulated = firstValue;
+
+    for (let i = 0; i < this.length; i++) {
+      acumulated = callback(acumulated, this[i], i, this);
+    }
+
+    return acumulated;
   };
 }
 
