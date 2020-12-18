a53f13102697e97490d06396004a8b10eebb6049
Lora Levitska <larakad94@gmail.com>
2020-12-18 22:38:03 +0200

    Implement method reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = initialValue !== undefined ? initialValue : this[0];
+    const startIndex = initialValue !== undefined ? 0 : 1;
+
+    for (let i = startIndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
