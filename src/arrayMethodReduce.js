9067bf7939d9d2243668f6a8acb70332fe85b82d
troospieler <troospieler1990@gmail.com>
2020-12-18 19:05:30 +0200

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,25 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result;
+
+    if (initialValue || initialValue === 0) {
+      result = initialValue;
+
+      for (let i = 0; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+      }
+    }
+
+    if (!initialValue && initialValue !== 0) {
+      result = this[0];
+
+      for (let i = 1; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+      }
+    }
+
+    return result;
   };
 }
 
