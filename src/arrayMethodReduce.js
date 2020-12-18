5c34ba009b42d09fa9ec119b82560be463e6c7c3
Nazar Valchyshyn <nvalja@gmail.com>
2020-12-18 20:57:53 +0200

    made array-metod-reduce solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,17 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result = initialValue;
+
+    if (initialValue === undefined) {
+      result = typeof (this[0]) === 'string' ? '' : this[0];
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
