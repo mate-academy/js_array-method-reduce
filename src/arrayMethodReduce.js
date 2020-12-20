21f68b2c7848fd06d441a1f123973821c1576b5b
tamara ostapets <tamara.ostapec@gmail.com>
2020-12-20 16:21:13 +0200

    Reduce Method Implementation

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,22 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let start;
+    let value;
+
+    if (initialValue !== undefined) {
+      value = initialValue;
+      start = 0;
+    } else {
+      value = this[0];
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      value = callback(value, this[i], i, this);
+    }
+
+    return value;
   };
 }
 
