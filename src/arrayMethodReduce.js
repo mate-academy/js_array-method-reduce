9a9d333004311b19c37d8a6659760c9f5e6fac93
Oleksii Spalylo <white.rh42@gmail.com>
2020-12-26 00:49:30 +0200

    implemented reduce method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let acc = initialValue;
+    let index = 0;
+
+    if (initialValue === undefined) {
+      acc = this[0];
+      index = 1;
+    }
+
+    for (let i = index; i < this.length; i++) {
+      acc = callback(acc, this[i], i, this);
+    }
+
+    return acc;
   };
 }
 
