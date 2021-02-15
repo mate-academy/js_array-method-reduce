3d4b7b04f616d9e0f369ee0e0ddf64d5ad0b6032
Bohdan Dobak <chudolipton@gmail.com>
2021-02-15 11:23:51 +0200

    fix

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let sum = initialValue;
+    let accumulator = initialValue;
     let startIndex = 0;
 
     if (arguments.length < 2) {
-      sum = this[0];
+      accumulator = this[0];
       startIndex = 1;
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      sum = callback(sum, this[i], i, this);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
-    return sum;
+    return accumulator;
   };
 }
 
