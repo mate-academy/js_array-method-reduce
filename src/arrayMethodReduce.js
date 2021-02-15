fa29c9c879b97b3e0e3ac09da64a4a81ecdc71cb
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-15 14:14:58 +0200

    first commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let accamulator = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      accamulator = this[0];
+      startIndex = 1;
+    };
+
+    for (let i = startIndex; i < this.length; i++) {
+      accamulator = callback(accamulator, this[i], i, this);
+    }
+
+    return accamulator;
   };
 }
 
