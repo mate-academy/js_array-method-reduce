e5e95b0e08747a7b9998064534f145c3700141f7
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-02-19 17:44:17 +0200

    first commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,21 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator = initialValue;
+
+    if (typeof initialValue !== 'undefined') {
+      for (let i = 0; i < this.length; i++) {
+        accumulator = callback(accumulator, this[i], i, this);
+      }
+    } else {
+      accumulator = this[0];
+
+      for (let i = 1; i < this.length; i++) {
+        accumulator = callback(accumulator, this[i], i, this);
+      }
+    }
+
+    return accumulator;
   };
 }
 
