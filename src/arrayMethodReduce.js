98f0d705088d8c18297e399d2b5af6156bb5e66e
Taras Chirkov <chirkovtaras@gmail.com>
2021-02-19 12:36:11 +0200

    fix

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -7,9 +7,7 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     let accumulator = initialValue;
-    let i;
-
-    (arguments.length === 1) ? i = 1 : i = 0;
+    let i = (arguments.length === 1) ? 1 : 0;
 
     for (i; i < this.length; i++) {
       accumulator = (callback(accumulator, this[i], i, this));
