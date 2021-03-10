818dabea42335675b2d4c73af7eceb1a3bc49661
Ivan Bilozerov <vaniabelozerov@gmail.com>
2021-03-10 22:10:30 +0200

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,23 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, initialValue = '') {
+    let sum;
+    let startValue;
+
+    if (typeof initialValue === 'string') {
+      sum = this[0];
+      startValue = 1;
+    } else {
+      sum = 0;
+      startValue = 0;
+    };
+
+    for (let i = startValue; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum || initialValue;
   };
 }
 
