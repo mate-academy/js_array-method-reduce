39dbf9cca2c05b299cf6128ae57289dd7fc467cc
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-11 18:36:00 +0200

    Created function applyCustomReduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     // write code here
+    let start = 0;
+    let result = initialValue;
+
+    if (initialValue === undefined) {
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
