20be386f92f2e86afa7c3d588bbebb69d7bf47b3
Max Zadorozhniy <max.zadorozhniy2001@gmail.com>
2021-03-12 13:11:57 +0200

    fixed else

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,15 +5,12 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let count;
-    let temp;
+    let count = initialValue;
+    let temp = 0;
 
     if (initialValue === undefined) {
       count = this[0];
       temp = 1;
-    } else {
-      count = initialValue;
-      temp = 0;
     }
 
     for (let i = temp; i < this.length; i++) {
