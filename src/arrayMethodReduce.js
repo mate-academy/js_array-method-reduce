db8cb7915f4fec6b8d035723081fe8871b82771a
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-04-28 22:18:06 +0300

    done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = this[0];
+    let pos = 1;
+
+    if (initialValue !== undefined) {
+      prev = initialValue;
+      pos = 0;
+    }
+
+    for (let i = pos; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
