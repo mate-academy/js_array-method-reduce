f5d6b54b18cd62a9f94e263fdfde2e7c7b89cccd
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-04-29 12:04:59 +0300

    variables were renamed

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = this[0];
-    let pos = 1;
+    let previous = this[0];
+    let index = 1;
 
     if (initialValue !== undefined) {
-      prev = initialValue;
-      pos = 0;
+      previous = initialValue;
+      index = 0;
     }
 
-    for (let i = pos; i < this.length; i++) {
-      prev = callback(prev, this[i], i, this);
+    for (let i = index; i < this.length; i++) {
+      previous = callback(previous, this[i], i, this);
     }
 
-    return prev;
+    return previous;
   };
 }
 
