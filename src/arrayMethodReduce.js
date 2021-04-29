5f296bb41446bb71e307e4943d8fa9b2ded82a0b
Dima Batiuk <batiuk20@gmail.com>
2021-04-29 23:55:13 +0300

    'solution'

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,22 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result = this[0];
+
+    if (initialValue === undefined) {
+      for (let i = 1; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+      }
+
+      return result;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      // eslint-disable-next-line no-param-reassign
+      initialValue = callback(initialValue, this[i], i, this);
+    }
+
+    return initialValue;
   };
 }
 
