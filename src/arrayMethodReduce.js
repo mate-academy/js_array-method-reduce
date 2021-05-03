163bd838b0613fdd2e6e06fae851f750de578ee2
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-05-03 20:06:00 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,34 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this.length === 0 && initialValue === undefined) {
+      throw new TypeError('array is empty');
+    }
+
+    if (this.length === 1 && initialValue === undefined) {
+      return this.filter(item => item !== undefined);
+    }
+
+    if (this.length === 0 && initialValue !== undefined) {
+      return initialValue;
+    }
+
+    let accumulator;
+    let startValue;
+
+    if (initialValue === undefined) {
+      accumulator = this[0];
+      startValue = 1;
+    } else {
+      accumulator = initialValue;
+      startValue = 0;
+    }
+
+    for (let i = startValue; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
