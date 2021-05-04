027e916cff31b7e48cb731b02bfce59437a81a12
Igor Lisogor <illisogori@gmail.com>
2021-05-04 17:22:19 +0300

    reduce method implemented

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const start = initialValue === undefined ? 1 : 0;
+    let accum = initialValue !== undefined ? initialValue : this[0];
+
+    for (let i = start; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return accum;
   };
 }
 
