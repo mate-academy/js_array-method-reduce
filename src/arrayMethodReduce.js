ab0045c636e040b1a76796289f08e72031bfeb57
Timur <hrynenko.timur@gmail.com>
2021-05-06 16:35:42 +0300

    solution refactored according to the comment  above

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,22 +3,24 @@
 /**
  * Implement method Reduce
  */
+
+//  let's assign default values to startIndex and only
+// if their is no initialValue reassign them
+//  no need for if else here
+//  also there should be only ONE for-loop
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev;
+    let prev = initialValue;
+    let startIndex = 0;
 
-    if (typeof initialValue !== 'undefined') {
-      prev = initialValue;
-
-      for (let i = 0; i < this.length; i++) {
-        prev = callback(prev, this[i], i, this);
-      }
-    } else {
+    if (typeof prev === 'undefined') {
       prev = this[0];
+      startIndex = 1;
+    }
 
-      for (let i = 1; i < this.length; i++) {
-        prev = callback(prev, this[i], i, this);
-      }
+    for (let i = startIndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
     }
 
     return prev;
