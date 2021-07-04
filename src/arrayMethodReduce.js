50c19dc29b401926d2d5930481e183ce29488005
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-07-04 13:26:20 +0300

    feat: completed task reduce-method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulatedValue = initialValue;
+    let startIndex = 0;
+
+    if (initialValue === undefined) {
+      startIndex = 1;
+      accumulatedValue = this[0];
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      accumulatedValue = callback(accumulatedValue, this[i], i, this);
+    }
+
+    return accumulatedValue;
   };
 }
 
