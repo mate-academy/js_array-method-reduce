d7b441a08b4a2a47bfc92dec47f15437ffaa7388
Sergei <serheifil@gmail.com>
2021-07-07 12:27:52 +0300

    was added default initialValue

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,14 +4,9 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     let accumulator = initialValue;
-    let beginIndex = 0;
-
-    if (initialValue === undefined) {
-      accumulator = this[0];
-      beginIndex = 1;
-    }
+    const beginIndex = initialValue === this[0] ? 1 : 0;
 
     for (let i = beginIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
