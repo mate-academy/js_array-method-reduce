d8c28a27affc4fbc0ac40833074bd5587a4ea1d7
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-07 13:42:00 +0300

    code was reduced a bit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,9 +6,8 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     let previousValue = initialValue !== undefined ? initialValue : this[0];
-    const idx = initialValue !== undefined ? 0 : 1;
 
-    for (let i = idx; i < this.length; i++) {
+    for (let i = initialValue !== undefined ? 0 : 1; i < this.length; i++) {
       previousValue = callback(previousValue, this[i], i, this);
     }
 
