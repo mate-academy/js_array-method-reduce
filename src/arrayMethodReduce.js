a2c2de0ac411fbef24259eab5ae75b2d51c0af56
NastyaVovk <winc4ester@gmail.com>
2021-08-09 17:18:58 +0300

    initial

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let previous = initialValue === undefined ? this[0] : initialValue;
+    const startIndex = initialValue === undefined ? 1 : 0;
+
+    for (let i = startIndex; i < this.length; i++) {
+      previous = callback(previous, this[i], i, this);
+    }
+
+    return previous;
   };
 }
 
