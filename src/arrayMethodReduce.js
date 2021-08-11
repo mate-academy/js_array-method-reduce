ba6393fa218cdaf8f46678301b669c9f9d67574d
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-11 17:42:44 +0300

    version 1.0

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
     // write code here
+    let result = initialValue;
+    let startIndex = 0;
+
+    if (initialValue === this[0]) {
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
