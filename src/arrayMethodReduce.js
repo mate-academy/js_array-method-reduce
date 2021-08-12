c3c25fe2af5a53b84b3b2f02ceb1dac1f9f22ddc
lera-yaskevych <redrum.back@gmail.com>
2021-08-12 13:56:42 +0300

    fixed

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -8,7 +8,7 @@ function applyCustomReduce() {
     let accumulator = initialValue;
     let currIndex = 0;
 
-    if (arguments.length < 2) {
+    if (initialValue === undefined) {
       accumulator = this[0];
       currIndex = 1;
     }
