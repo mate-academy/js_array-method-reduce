edbbdcd3745b50eac304932b1d400a7a89a2828c
Vova-Stoliar <stoliarmoney@gmail.com>
2021-08-12 17:52:12 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,20 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let result = initialValue;
+    let index = 0;
+
+    if (initialValue === undefined) {
+      result = this[0];
+      index = 1;
+    }
+
+    while (index < this.length) {
+      result = callback(result, this[index], index, this);
+      index++;
+    }
+
+    return result;
   };
 }
 
