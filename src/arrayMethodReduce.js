0816aaeaeafb1ee84a34c221b09878ae034097df
zadorojniiarsen <zadorojniiarsen@gmail.com>
2021-08-23 21:34:16 +0300

    firt try

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].proto.reduce2 = function(callback, initialValue) {
+    let currentValue = initialValue;
+    let index = 0;
+
+    if (initialValue === undefined) {
+      currentValue = this[0];
+      index = 1;
+    }
+
+    for (let i = index; i < this.length; i++) {
+      currentValue = callback(currentValue, this[i], i, this);
+    }
+
+    return currentValue;
   };
 }
 
