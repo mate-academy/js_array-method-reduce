4645589a84195691823d8eb53bfa84e187a82743
Anton Kulchytskyi <antek1809@gmail.com>
2021-09-13 17:42:55 +0300

    Changes added

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,14 +6,16 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let y = 0;
     let sum = initialValue;
 
-    for (let i = 0; i < this.length; i++) {
-      if (sum !== undefined) {
-        sum = callback(sum, this[i], i, this);
-      } else {
-        sum = this[i];
-      }
+    if (initialValue === undefined) {
+      y = 1;
+      sum = this[0];
+    }
+
+    for (let i = y; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
     }
 
     return sum;
