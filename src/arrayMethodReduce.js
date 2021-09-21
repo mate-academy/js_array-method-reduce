68f5907f938d9e7e1650289f56eeccbd712ed6ee
Kostiantyn Besedin <besconstantine@gmail.com>
2021-09-21 17:17:43 +0300

    fixes after code review

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,15 +4,15 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue = 0) {
+  [].__proto__.reduce2 = function(callback, initialValue) {
     const start = arguments.length > 1 ? 0 : 1;
-    let startSum = arguments.length > 1 ? initialValue : this[0];
+    let concatination = arguments.length > 1 ? initialValue : this[0];
 
     for (let i = start; i < this.length; i++) {
-      startSum = callback(startSum, this[i], i, this);
+      concatination = callback(concatination, this[i], i, this);
     }
 
-    return startSum;
+    return concatination;
   };
 }
 
