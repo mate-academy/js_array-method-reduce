0d88301270377841cbe6273faea2b2280c24bd25
Taras <43114982+Tarasulko@users.noreply.github.com>
2021-09-29 14:53:24 +0300

    Update src/arrayMethodReduce.js
    Co-authored-by: Ihor Karpyn <63306833+Ihor-Karpyn@users.noreply.github.com>

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,12 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialVal) {
-    let accumulator = (initialVal === undefined) ? this[0] : initialVal;
-    const start = (initialVal === undefined) ? 1 : 0;
+    let accumulator = initialVal === undefined
+      ? this[0]
+      : initialVal;
+    const start = initialVal === undefined
+    	? 1
+    	: 0;
 
     for (let i = start; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
