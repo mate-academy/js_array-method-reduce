23bb817f4eaab456c285e859f0fa17ff3463081b
cyb1ove <cyb1ove.ua@gmail.com>
2021-10-14 10:59:39 +0300

    Task solved

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,21 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let start, iter;
+
+    if (initialValue !== undefined) {
+      start = initialValue;
+      iter = 0;
+    } else {
+      start = this[0];
+      iter = 1;
+    }
+
+    for (let i = iter; i < this.length; i++) {
+      start = callback(start, this[i], i, this);
+    }
+
+    return start;
   };
 }
 
