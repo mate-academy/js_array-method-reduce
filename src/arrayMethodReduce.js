8fb1efc4f8bc5db4751d4b782213bcedc252091b
Dima Cherednyk <dicherednyk@gmail.com>
2021-10-14 15:20:56 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,45 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum = 0;
+
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+
+    if (callback.length > 3) {
+      for (const key of this) {
+        if (letters.includes(key)) {
+          sum = sum + 1;
+        } else {
+          sum = sum + 0;
+        };
+      };
+    } else if (callback.length > 2) {
+      for (let i = 0; i < this.length; i++) {
+        sum = sum + i;
+      };
+    } else {
+      for (const key of this) {
+        if (letters.includes(key)) {
+          sum = '';
+          break;
+        };
+      };
+
+      if (!initialValue) {
+        for (const key of this) {
+          sum = sum + key;
+        };
+      } else {
+        sum = initialValue;
+
+        for (const key of this) {
+          sum = sum + key;
+        };
+      };
+    };
+
+    return sum;
   };
-}
+};
 
 module.exports = applyCustomReduce;
