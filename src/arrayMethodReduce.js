d1253633139ae4c6028b2fe916eba91c552a9904
Igor Kovalev <bizzzy0@gmail.com>
2021-10-14 11:33:26 +0300

    Task was solved

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,11 +1,21 @@
+/* eslint-disable no-unused-expressions */
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let previous = initialValue;
+    let startIndex;
+
+    arguments.length < 2 ? (
+      previous = this[0],
+      startIndex = 1
+    ) : startIndex = 0;
+
+    for (let i = startIndex; i < this.length; i++) {
+      previous = callback(previous, this[i], i, this);
+    }
+
+    return previous;
   };
 }
 
