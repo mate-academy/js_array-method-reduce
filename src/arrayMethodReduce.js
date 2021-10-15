9ddef5dfc7bdec3f76b24ed986f89f807261f840
cyb1ove <cyb1ove.ua@gmail.com>
2021-10-15 21:36:33 +0300

    Method was rewrited

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,24 +3,18 @@
 /**
  * Implement method Reduce
  */
+
+
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
-    let start, iter;
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
+    let result = initialValue;
+    const startIndex = arguments.length < 2 ? 1 : 0;
 
-    if (initialValue !== undefined) {
-      start = initialValue;
-      iter = 0;
-    } else {
-      start = this[0];
-      iter = 1;
+    for (let i = startIndex; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
     }
 
-    for (let i = iter; i < this.length; i++) {
-      start = callback(start, this[i], i, this);
-    }
-
-    return start;
+    return result;
   };
 }
 
