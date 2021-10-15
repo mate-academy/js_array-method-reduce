17552ac064da90136393c85276aef3338c0e9110
Mariia Obraztsova <mariiaobraztsova99@gmail.com>
2021-10-15 18:51:56 +0300

    Fixed method 2.0

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,13 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator = (initialValue !== undefined) ? initialValue : this[0];
-    let index = (initialValue !== undefined) ? 0 : 1;
+    let accumulator = initialValue;
+    let index = 0;
+
+    if (arguments.length < 2) {
+      accumulator = this[0];
+      index = 1;
+    }
 
     for (index; index < this.length; index++) {
       accumulator = callback(accumulator, this[index], index, this);
