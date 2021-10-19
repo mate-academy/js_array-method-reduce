cc2bbd8336fba8701773c5f5ef54eb01ec79b6a1
Dima Cherednyk <dicherednyk@gmail.com>
2021-10-19 22:54:10 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,45 +1,23 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let sum = 0;
+    let sum = '';
 
-    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    if (initialValue) {
+      sum = initialValue;
+    };
 
-    if (callback.length > 3) {
-      for (const key of this) {
-        if (letters.includes(key)) {
-          sum = sum + 1;
-        } else {
-          sum = sum + 0;
-        };
-      };
-    } else if (callback.length > 2) {
-      for (let i = 0; i < this.length; i++) {
-        sum = sum + i;
-      };
-    } else {
-      for (const key of this) {
-        if (letters.includes(key)) {
-          sum = '';
-          break;
-        };
-      };
+    for (let i = 0; i < 1; i++) {
+      sum = callback(sum, this[i], i, this);
+    };
 
-      if (!initialValue) {
-        for (const key of this) {
-          sum = sum + key;
-        };
-      } else {
-        sum = initialValue;
+    if (Number.isInteger(Number(sum))) {
+      sum = Number(sum);
+    };
 
-        for (const key of this) {
-          sum = sum + key;
-        };
-      };
+    for (let i = 1; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
     };
 
     return sum;
