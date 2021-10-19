e742df4d57a8961483f05af75d57fc587173b17a
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-10-19 16:12:15 +0300

    commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,10 +4,10 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  
   [].__proto__.reduce2 = function(callback, initialValue) {
     let startValue;
     let index;
+
     if (arguments.length === 1) {
       startValue = this[0];
       index = 1;
@@ -15,13 +15,13 @@ function applyCustomReduce() {
       startValue = initialValue;
       index = 0;
     }
-    
-    
+
     for (let i = index; i < this.length; i++) {
       startValue = callback(startValue, this[i], i, this);
     }
+
     return startValue;
-  }
+  };
 }
 
 module.exports = applyCustomReduce;
