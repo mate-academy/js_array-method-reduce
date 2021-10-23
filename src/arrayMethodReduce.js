980263ee20231ca1389f1b9fdd839fd521428cf7
Yuriy Titov <yuranus@gmail.com>
2021-10-23 09:18:41 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,18 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prevElement = arguments.length === 1
+      ? this[0]
+      : initialValue;
+    let index = arguments.length === 1
+      ? 1
+      : 0;
+
+    for (index; index < this.length; index++) {
+      prevElement = callback(prevElement, this[index], index, this);
+    }
+
+    return prevElement;
   };
 }
 
