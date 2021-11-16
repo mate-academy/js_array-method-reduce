db76f92d0bdb1d0e30b7e8ac924a454f41f1b2d5
Ihor Poliakov <junglqq@gmail.com>
2021-11-16 16:20:44 +0200

    init

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,22 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function (callback, initialValue) {
+    let accum = initialValue;
+    let beginIndex = 0;
+
+    if (arguments.length === 1) {
+      accum = this[0];
+      beginIndex = 1;
+    }
+
+    for (let index = beginIndex; index < this.length; index++) {
+      const currentValue = this[index];
+
+      accum = callback(accum, currentValue, index, this);
+    }
+
+    return accum;
   };
 }
 
