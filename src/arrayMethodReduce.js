d1c375b3182872fb7b1da0fe381b329bff4abf1e
WorkAccCom <work.acc.com@gmail.com>
2021-11-18 14:20:14 +0200

    Two tests failed

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,14 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
+    let prevCallResult = initialValue;
+
+    for (let i = 1; i < this.length; i++) {
+      prevCallResult = callback(prevCallResult, this[i], i, this);
+    }
+
+    return prevCallResult;
   };
 }
 
