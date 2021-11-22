3a0c3bf3468e3163eeddb0a2bf165a6843717649
Dmitriy Ziakhor <dmziakhor@gmail.com>
2021-11-22 11:13:41 +0200

    Fixes

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,21 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let result;
-    let startIndex = 0;
-
-    if (arguments.length === 1) {
-      result = this[0];
-      startIndex++;
-    } else {
-      result = initialValue;
-    }
+    const startIndex = arguments.length === 1 ? 1 : 0;
+    let accumulator = arguments.length === 1 ? this[0] : initialValue;
 
     for (let i = startIndex; i < this.length; i++) {
-      result = callback(result, this[i], i, this);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
-    return result;
+    return accumulator;
   };
 }
 
