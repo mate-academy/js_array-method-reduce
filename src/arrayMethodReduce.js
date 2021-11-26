b5ffb750341a1f38d7e1fb2613ecbc6e7d72209f
Vladyslav <vladyslav3177@gmail.com>
2021-11-26 21:08:27 +0200

    reduce method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,21 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let firstValue = initialValue;
+    let start;
+
+    if (arguments.length === 1) {
+      firstValue = this[0];
+      start = 1;
+    } else {
+      start = 0;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      firstValue = callback(firstValue, this[i], i, this);
+    }
+
+    return firstValue;
   };
 }
 
