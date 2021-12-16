ae4051386d13e08217ea03efbdfbc587acb0d364
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-16 14:21:03 +0200

    commit added

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,17 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum = initialValue;
+
+    if (arguments.length < 2) {
+      sum = this[0];
+    }
+
+    for (let i = +(arguments.length < 2); i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum;
   };
 }
 
