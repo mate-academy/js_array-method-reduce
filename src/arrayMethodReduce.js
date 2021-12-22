f8e46b85795bfcd2e9a54e081dc11db86c61ced9
Taras Zherebetkiy <tarasj76@gmail.com>
2021-12-22 13:08:11 +0200

    reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let start = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      start = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      start = callback(start, this[i], i, this);
+    }
+
+    return start;
   };
 }
 
