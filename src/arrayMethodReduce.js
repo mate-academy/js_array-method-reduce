e107ce736b42db53ac09519213ca017ab4f794d7
Yulia Koshkina <julyasav4uk@gmail.com>
2021-12-23 08:29:05 +0200

    first commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev
+      = (arguments.length < 2) ? this[0] : initialValue;
+    const start
+      = (arguments.length < 2) ? 1 : 0;
+
+    for (let i = start; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
