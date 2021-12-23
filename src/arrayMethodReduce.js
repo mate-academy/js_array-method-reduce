9af45348721ca306e89ba3ebb87862f448a8a10b
Taras Zherebetkiy <tarasj76@gmail.com>
2021-12-23 16:19:00 +0200

    change variable name

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let start = initialValue;
+    let prevValue  = initialValue;
     let startIndex = 0;
 
     if (arguments.length < 2) {
-      start = this[0];
+      prevValue  = this[0];
       startIndex = 1;
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      start = callback(start, this[i], i, this);
+      prevValue  = callback(prevValue , this[i], i, this);
     }
 
-    return start;
+    return prevValue ;
   };
 }
 
