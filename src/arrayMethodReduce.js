c380e17151d7c90ae96994eaf63aba9b138225bb
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-24 14:48:59 +0200

    wow

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = this[0];
+    let startIndex = 1;
+
+    if (arguments.length > 1) {
+      prev = initialValue;
+      startIndex = 0;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
