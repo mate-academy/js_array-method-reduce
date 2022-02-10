27329226f6e2bdcf482587da116883549e00f277
Roma Afanasiev <romafanasiev123@gmail.com>
2022-02-10 22:42:23 +0200

    add function

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,7 +6,28 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    const t = Object(this);
+    const len = t.length;
+    let k = 0;
+    let value;
+
+    if (arguments.length >= 2) {
+      value = arguments[1];
+    } else {
+      while (k < len && !(k in t)) {
+        k++;
+      }
+      value = t[k++];
+    }
+
+    for (k; k < len; k++) {
+      if (k in t) {
+        value = callback(value, t[k], k, t);
+      }
+    }
+
+    return value;
   };
-}
+};
 
 module.exports = applyCustomReduce;
