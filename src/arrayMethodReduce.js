9e819a02136e7d90b7839f8f89918c895dc26138
Max Shlikhta <maxok.vibe@gmail.com>
2022-02-16 19:51:48 +0200

    Reduce method implementation

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let storage = initialValue;
+    let startFromIndex = 0;
+
+    if (arguments.length < 2) {
+      storage = this[0];
+      startFromIndex = 1;
+    }
+
+    for (let i = startFromIndex; i < this.length; i++) {
+      storage = callback(storage, this[i], i, this);
+    }
+
+    return storage;
   };
 }
 
