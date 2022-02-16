075d4ebddc6f0d14f876f46659ae72081fb95889
Anna Serdechna <anserdechna@gmail.com>
2022-02-16 20:09:17 +0200

    change var name

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
+    let accumulator  = initialValue;
     let startIndex = 0;
 
     if (arguments.length < 2) {
-      prev = this[0];
+      accumulator  = this[0];
       startIndex = 1;
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      prev = callback(prev, this[i], i, this);
+      accumulator  = callback(accumulator , this[i], i, this);
     }
 
-    return prev;
+    return accumulator ;
   };
 }
 
