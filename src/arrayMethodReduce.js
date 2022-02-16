8e1390994f5430c6d238a3338a0df5a6910217e8
Anton Hanetskyi <antonganetskiy@gmail.com>
2022-02-16 22:05:51 +0200

    variable name  prev was changed to accumulator

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
+    let accumulator = initialValue;
     let start = 0;
 
     if (arguments.length < 2) {
-      prev = this[0];
+      accumulator = this[0];
       start = 1;
     }
 
     for (let i = start; i < this.length; i++) {
-      prev = callback(prev, this[i], i, this);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
-    return prev;
+    return accumulator;
   };
 }
 
