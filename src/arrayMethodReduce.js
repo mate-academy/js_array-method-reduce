5d1daad4920e337f8a75db37090920bd828ea756
Yurii Tyslenok <tislenok91@gmail.com>
2022-02-17 15:29:23 +0200

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = initialValue;
+    let num = 0;
+
+    if (arguments.length <= 1) {
+      prev = this[0];
+      num = 1;
+    }
+
+    for (let i = num; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
