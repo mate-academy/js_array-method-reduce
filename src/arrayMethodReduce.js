6be72705adf6c4a2af79b39dc5a2ab830d9ba1ac
Max Shylenko <maxshylenko@gmail.com>
2022-03-25 15:51:46 +0100

    arrayMethodReduce is done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,25 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this.length === 0) {
+      return initialValue;
+    }
+
+    let result;
+    let startValue = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length === 1) {
+      startValue = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      result = callback(startValue, this[i], i, this);
+      startValue = result;
+    }
+
+    return result;
   };
 }
 
