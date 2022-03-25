06e849b8ffaf7c1ccee4499e9c4ab33021af15a6
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-03-25 19:45:25 +0200

    js-array-method-reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,20 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const args = [...arguments];
+    let startValue = initialValue;
+    let startIndex = 0;
+
+    if (args.length === 1) {
+      startIndex = 1;
+      startValue = this[0];
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      startValue = callback(startValue, this[i], i, this);
+    }
+
+    return startValue;
   };
 }
 
