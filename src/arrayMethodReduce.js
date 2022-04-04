de4292d60890875ee19de191975c862990a955e4
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-04 14:41:11 +0300

    renamed variables

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -8,15 +8,15 @@ function applyCustomReduce() {
     const start = arguments.length < 2
       ? 1
       : 0;
-    let final = arguments.length < 2
+    let finalValue = arguments.length < 2
       ? this[0]
       : initialValue;
 
     for (let i = start; i < this.length; i++) {
-      final = callback(final, this[i], i, this);
+      finalValue = callback(finalValue, this[i], i, this);
     }
 
-    return final;
+    return finalValue;
   };
 }
 
