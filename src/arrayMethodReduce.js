f5d2922c28789fac310b906e0a1fcaeb87fa5b23
Artem Motalov <artemmmmmotalov@gmail.com>
2022-04-06 16:01:33 +0300

    Initial commit - solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,10 +3,40 @@
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const startValue = arguments.length <= 1
+      ? 1
+      : 0;
+
+    let previousValue = arguments.length <= 1
+      ? this[0]
+      : initialValue;
+
+    for (let i = startValue; i < this.length; i++) {
+      previousValue = callback(previousValue, this[i], i, this);
+    }
+
+    return previousValue;
   };
 }
+
+/* function applyCustomReduce() {
+  [].__proto__.reduce2 = function(callback, initialValue) {
+    const start = arguments.length < 2
+      ? 1
+      : 0;
+    let finalValue = arguments.length < 2
+      ? this[0]
+      : initialValue;
+
+    for (let i = start; i < this.length; i++) {
+      finalValue = callback(finalValue, this[i], i, this);
+    }
+
+    return finalValue;
+  };
+} */
 
 module.exports = applyCustomReduce;
