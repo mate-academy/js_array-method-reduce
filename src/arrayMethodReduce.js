c450e580f25661b59139a9cd088ab2227f77c60a
yurii837 <gorogotskiy@gmail.com>
2022-04-08 18:11:26 +0300

    done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,23 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = initialValue;
+    let start = 0;
+
+    if (arguments.length < 2) {
+      prev = this[0];
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      const curr = this[i];
+      const ind = i;
+      const arr = this;
+
+      prev = callback(prev, curr, ind, arr);
+    }
+
+    return prev;
   };
 }
 
