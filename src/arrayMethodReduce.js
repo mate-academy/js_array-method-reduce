302fe4ac3815fb1c67740b00d94e14e7d03eceec
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-04-11 14:20:00 +0300

    Delete else argument

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -11,8 +11,6 @@ function applyCustomReduce() {
     if (arguments.length < 2) {
       prev = this[0];
       startIndex = 1;
-    } else if (typeof initialValue === 'undefined') {
-      prev = `undefined`;
     }
 
     for (let i = startIndex; i < this.length; i++) {
