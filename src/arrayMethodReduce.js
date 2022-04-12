56c6bf4dd6eb8ff223bb100d5777a40027d138e9
unknown <avicostov2@gmail.com>
2022-04-12 16:59:43 +0200

    solut

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let init = initialValue;
+    let index = 0;
+
+    if (arguments.length < 2) {
+      init = this[0];
+      index = 1;
+    }
+
+    for (let i = index; i < this.length; i++) {
+      init = callback(init, this[i], i, this);
+    }
+
+    return init;
   };
 }
 
