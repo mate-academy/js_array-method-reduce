7b00f0cebe5342c595cf18b294473839eda81416
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-02 10:34:51 +0300

    arrayReduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,20 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+
+    let prev = initialValue;
+    let startInd = 0;
+
+    if (arguments.length < 2) {
+      prev = this[0];
+      startInd = 1;
+    }
+
+    for (let i = startInd; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
