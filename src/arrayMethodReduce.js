0f3876ac1d46c0cdfc45f5bac3d28e39a768094d
MabuzaM <mabuzamfano@gmail.com>
2022-05-03 14:47:09 +0200

    Need to figure out why the other 2 tests are failing

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,18 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, initialValue = this[0]) {
+    let previousValue = initialValue;
+
+    if (initialValue === undefined) {
+      previousValue = undefined;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      previousValue = callback(previousValue, this[i], i, this);
+    }
+
+    return previousValue;
   };
 }
 
