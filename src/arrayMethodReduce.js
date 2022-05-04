b772e72855e3734ba8e760e98205bd851d13dce9
Roman Pavlenko <proman9973@gmail.com>
2022-05-04 20:16:50 +0300

    solutionI

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,42 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let accum;
+
+    if (arguments.length === 1) {
+      if (typeof (this[0]) === 'string') {
+        accum = '';
+      } else if (typeof (this[0]) === 'number') {
+        accum = this[0];
+      }
+    } else {
+      accum = initialValue;
+    }
+
+    switch (callback.length) {
+      case 2: {
+        for (let i = 0; i < this.length; i++) {
+          accum = callback(accum, this[i]);
+        }
+        break;
+      }
+
+      case 3: {
+        for (let i = 0; i < this.length; i++) {
+          accum = callback(accum, this[i], i);
+        }
+        break;
+      }
+
+      case 4: {
+        for (let i = 0; i < this.length; i++) {
+          accum = callback(accum, this[i], i, this);
+        }
+        break;
+      }
+    }
+
+    return accum;
   };
 }
 
