aac9430d2c0df8493120fdf20ab42287f9855b3f
Andrei Sucman <67225216+andreisucman@users.noreply.github.com>
2022-05-06 14:24:30 +0300

    Update src/arrayMethodReduce.js
    Co-authored-by: Ihor Nahnybida <79282749+ihor-jpeg@users.noreply.github.com>

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,7 +6,9 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     let prev = arguments.length < 2 ? this[0] : initialValue;
-    const startIndex = arguments.length < 2 ? 1 : 0;
+    const startIndex = arguments.length < 2
+      ? 1
+      : 0;
 
     for (let i = startIndex; i < this.length; i++) {
       prev = callback(prev, this[i], i, this);
