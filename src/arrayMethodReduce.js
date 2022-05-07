d491cf5abfcaa97e1833c3ae0ec561d0028e4976
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-05-07 15:45:02 +0300

    solve the arrayMathodReduce problem

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,15 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const initialValuePassed = arguments.length === 2;
+
+    let sum = initialValuePassed ? initialValue : this[0];
+
+    for (let i = initialValuePassed ? 0 : 1; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum;
   };
 }
 
