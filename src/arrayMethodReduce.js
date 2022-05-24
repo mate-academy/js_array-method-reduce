f4c3333a77d78317280482598a710aac70609e25
Max <59259022+WhiteAshDc@users.noreply.github.com>
2022-05-24 19:41:14 +0200

    result

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let start = initialValue;
+    let startI = 0;
+
+    if (arguments.length < 2) {
+      startI = 1;
+      start = this[0];
+    }
+
+    for (let i = startI; i < this.length; i++) {
+      start = callback(start, this[i], i, this);
+    }
+
+    return start;
   };
 }
 
