2541eb2dab6cfe9a74d946e58330e11e20e499e4
Artem Furdela <furdela.artem@gmail.com>
2022-06-05 23:00:34 +0300

    Reduce commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let startValue = initialValue;
+    let start = 0;
+
+    if (arguments.length < 2) {
+      startValue = this[0];
+      start = 1;
+    }
+
+    for(let i = start; i < this.length; i++) {
+      startValue = callback(startValue, this[i], i, this);
+    }
+
+    return startValue;
   };
 }
 
