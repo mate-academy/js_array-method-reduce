a72536874bd744c2d52e88307365b433180b593f
DimaShm <shmagindmitry@gmail.com>
2022-06-12 13:35:37 +0300

    Array metod sort solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let summ = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      summ = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      summ = callback(summ, this[i], i, this);
+    }
+
+    return summ;
   };
 }
 
