66da832a080aefeeadef5e9c482ab4c5b89e4bec
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-06-14 18:16:34 +0300

    first version

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,20 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum = initialValue;
+    let count = 0;
+
+    if (arguments.length < 2) {
+      sum = this[0];
+      count++;
+    }
+
+    while (count < this.length) {
+      sum = callback(sum, this[count], count, this);
+      count++;
+    }
+
+    return sum;
   };
 }
 
