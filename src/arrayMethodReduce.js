93ee914e86cdce7a51cadbf1cb0b1a211e84449d
Kamil Witek <kamilprogramuje@gmail.com>
2022-06-16 23:17:53 +0200

    solution ver 2

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,37 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue = '') {
+  [].__proto__.reduce2 = function(callback, initialValue) {
     let sum = initialValue;
-    // if given array contains only strings
-    let startValue = '';
+    let startIndex = 0;
 
-    if (typeof this[0] === 'number') {
-      startValue = 0;
+    if (arguments.length < 2) {
+      startIndex = 1;
+      sum = this[0];
     }
 
-    // when passed two arguments and equal to undefined
-    if (arguments.length === 2 && arguments[1] === undefined) {
-      sum = undefined;
+    for (let i = startIndex; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
     }
 
-    // when passed only one argument and type of array is number
-    if (arguments.length === 1 && startValue === 0) {
-      sum = 0;
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      // convert to number
-      let next = parseInt(callback(startValue, this[i], i, this));
-
-      // handle as string
-      if (isNaN(next)) {
-        next = callback(startValue, this[i], i, this);
-      }
-      sum += next;
-    }
-
-    return sum;
+    return sum || initialValue;
   };
 }
 
