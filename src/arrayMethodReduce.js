6668372ad7bbfaf2bfdf1cd67500b27bfac7b951
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-21 00:36:14 +0300

    second commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,31 +3,19 @@
 /**
  * Implement method Reduce
  */
-
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let result = initialValue;
+    let start = 0;
 
-    let result;
-
-    const forCycle = (variable) => {
-      for (let i = variable; i < this.length; i++) {
-        result = callback(result, this[i], i, this);
-      }
-    };
-
-    if (initialValue !== undefined) {
-      result = initialValue;
-      forCycle(0);
-    } else if (
-      initialValue === undefined
-      && arguments.length >= 2
-    ) {
-      result = `undefined`;
-      forCycle(0);
-    } else {
+    if (initialValue === undefined && arguments.length < 2) {
       result = this[0];
-      forCycle(1);
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
     }
 
     return result;
