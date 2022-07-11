58b2b5da43255f6b662d73ccff0e693ffe7efa34
Oleg Vetrov <oleg94038@gmail.com>
2022-07-11 15:19:47 +0300

    Implemented method Reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, ...initialValue) {
+    let result = this[0];
+    let index = 1;
+
+    if (initialValue.length > 0) {
+      result = initialValue[0];
+      index = 0;
+    }
+
+    for (index; index <= this.length - 1; index++) {
+      result = callback(result, this[index], index, this);
+    }
+
+    return result;
   };
 }
 
