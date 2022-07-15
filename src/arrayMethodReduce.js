731f4c351a8d7289d1b9838380feea905df55c77
Volodya <balyshev.volodya@gmail.com>
2022-07-15 12:22:21 +0300

    fixed after reject

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,14 +5,12 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev;
+    let prev = initialValue;
     let startIndex = 0;
 
     if (arguments.length < 2) {
       prev = this[0];
       startIndex = 1;
-    } else {
-      prev = initialValue;
     }
 
     for (let i = startIndex; i < this.length; i++) {
