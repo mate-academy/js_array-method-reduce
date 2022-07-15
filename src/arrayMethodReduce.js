ee39b14e30f649cf6a0dfd87efa14f68e8fa0158
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-07-15 12:43:10 +0300

    1-st commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum = initialValue;
+    let i = 0;
+
+    if (arguments.length === 1) {
+      sum = this[0];
+      i = 1;
+    }
+
+    for (i; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum;
   };
 }
 
