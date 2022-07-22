dfe4a7aabe8184a02f8d0bb65dc28f9cd960f402
Yura Atamaniuk <atam3905@gmail.com>
2022-07-22 13:05:47 +0300

    reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * Implement method Reduce
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let result = initialValue;
+    let start = 0;
+
+    if (initialValue === undefined && arguments.length < 2) {
+      result = this[0];
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
