a050754f2e7c6e6ebcb09de73fe8c0e0776359b4
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-04 20:52:54 +0300

    Task solution code

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const startIndex = arguments.length === 1 ? 1 : 0;
+    let prev = startIndex ? this[0] : initialValue;
+
+    for (let i = startIndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
