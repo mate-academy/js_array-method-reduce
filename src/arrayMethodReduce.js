bace0429144159caa31ec2008f5d66f3efa0fb15
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-08-11 16:34:18 +0300

    First commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,22 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, initialValue = 'undefined') {
+    let i = 0;
+    let previousValue = initialValue;
+
+    if (arguments.length === 1) {
+      previousValue = this[0];
+      i = 1;
+    }
+
+    let result = previousValue;
+
+    for (; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
