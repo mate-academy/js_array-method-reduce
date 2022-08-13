679f01e64c45acb9af065f9bb96dfe2e9be9e8a2
Boris Kerekesha <kerekesha90@ukr.net>
2022-08-13 15:43:46 +0300

    Reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let rezult = initialValue;
+    let indexBegin = 0;
+
+    if (arguments.length < 2) {
+      indexBegin = 1;
+      rezult = this[0];
+    }
+
+    for (let i = indexBegin; i < this.length; i++) {
+      rezult = callback(rezult, this[i], i, this);
+    }
+
+    return rezult;
   };
 }
 
