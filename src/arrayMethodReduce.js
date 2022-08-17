ef10120d8042240e87fc0facfc9669862cd9c00a
Maks Sobko <tiserettt@gmail.com>
2022-08-17 11:19:32 +0300

    changes after mentor review

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,8 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let previous = initialValue;
-    let startIndex = 0;
-
-    if (arguments.length < 2) {
-      previous = this[0];
-      startIndex = 1;
-    }
+    let previous = (arguments.length < 2) ? this[0] : initialValue;
+    const startIndex = (arguments.length < 2) ? 1 : 0;
 
     for (let i = startIndex; i < this.length; ++i) {
       previous = callback(previous, this[i], i, this);
