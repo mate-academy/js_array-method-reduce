365f37153b8d92955832c2261e86bf5725fe5c02
Oleh Mykolaichuk <ooolehmy@gmail.com>
2022-08-17 12:24:44 +0300

    var renamed

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,13 +6,13 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     const start = (arguments.length === 1) ? 1 : 0;
-    let previusValue = (arguments.length === 1) ? this[0] : initialValue;
+    let previousValue = (arguments.length === 1) ? this[0] : initialValue;
 
     for (let i = start; i < this.length; i++) {
-      previusValue = callback(previusValue, this[i], i, this);
+      previousValue = callback(previousValue, this[i], i, this);
     }
 
-    return previusValue;
+    return previousValue;
   };
 }
 
