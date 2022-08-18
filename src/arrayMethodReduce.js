cb280c6f5cdffc30d99048c8a40142afe7c13b0d
NataliyaAn <otchet333@gmail.com>
2022-08-18 15:46:04 +0300

    solution-reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const start = (arguments.length < 2) ? 1 : 0;
+    let step = (arguments.length < 2) ? this[0] : initialValue;
+
+    for (let i = start; i < this.length; i++) {
+      step = callback(step, this[i], i, this);
+    }
+
+    return step;
   };
 }
 
