eaa2bc50415ac3157acb7237e830f318931efaf8
Andrii Dmytruk <andriydmitr2014@gmail.com>
2022-08-18 10:49:39 +0300

    Done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = arguments.length !== 1
+      ? callback(initialValue, this[0], 0, this) : this[0];
+
+    for (let i = 1; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
+applyCustomReduce();
 
 module.exports = applyCustomReduce;
