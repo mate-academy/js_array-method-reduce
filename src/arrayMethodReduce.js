2e525054156bc5eaeb00a3c4b4f9297e5d194f03
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-08-26 18:48:50 +0800

    reduce done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,12 +1,38 @@
 'use strict';
-
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let j = 0;
+    let accum = 0;
+
+    if (arguments.length >= 2) { accum = initialValue; }
+
+    if (arguments.length < 2) {
+      accum = this[0];
+      j++;
+    }
+
+    for (let i = j; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return accum;
   };
 }
 
+applyCustomReduce();
+
 module.exports = applyCustomReduce;
+
+// numbers.map = function(callback) {
+//   const result = [];
+
+//   for (let i = 0; i < this.length; i++) {
+//     result[i] = callback(this[i], i, this);
+//   }
+
+//   return result;
+// };
