ac616910eed5eccdb7d13d887035aaec1ee67bb8
Max Bovtun <maxbovtun@gmail.com>
2022-09-13 16:29:13 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,9 +3,28 @@
 /**
  * Implement method Reduce
  */
+// - Here is a base realisation
+// |-- of .reduce() JS method ;)
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(
+    callback = ((a, b) => a + b),
+    initialValue,
+  ) {
+    let result = initialValue;
+
+    if (arguments.length < 2
+      && typeof this[0] === 'number') {
+      result = 0;
+    } else if (arguments.length < 2
+      && typeof this[0] === 'string') {
+      result = '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
