92703920200a247f0ef6e51a891ce21f246bca07
Aleks <offsset@gmail.com>
2022-09-15 15:26:43 +0300

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -12,19 +12,6 @@ function applyCustomReduce() {
       i = 1;
       result = this[0];
     }
-    
-    // if (arguments.length === 2 && ) {
-    //   i = 1;
-    //   result = this[0];
-    // }
-
-
-
-
-
-
-
-
 
     for (i; i < this.length; i++) {
       result = callback(result, this[i], i, this);
