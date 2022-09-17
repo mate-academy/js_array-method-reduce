e59ca3aa34a5ea09ae9ae133cfd8cf8b66ef0f77
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-17 11:03:03 +0300

    reduce method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,22 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulated = initialValue;
+    let currentElementIndex = 0;
+
+    if (arguments.length < 2) {
+      accumulated = this[0];
+      currentElementIndex = 1;
+    }
+
+    for (let i = currentElementIndex; i < this.length; i++) {
+      const currentElement = this[i];
+
+      accumulated = callback(accumulated, currentElement, i, this);
+    }
+
+    return accumulated;
   };
-}
+};
 
 module.exports = applyCustomReduce;
