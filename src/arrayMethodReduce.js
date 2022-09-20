536c5b003dd463fe274b7ec1b841a26cc7f2258a
fatihsamur <a.fatihsamur@gmail.com>
2022-09-21 01:40:18 +0300

    a little bit shorter

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -7,28 +7,17 @@ function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     let previousValue = initialValue;
 
-    if (!initialValue) {
+    if (arguments.length === 1) {
       previousValue = this[0];
-    }
 
-    if (arguments.length === 2
-        && (typeof initialValue === 'undefined' || initialValue === 0)) {
-      if (typeof initialValue === 'undefined') {
-        previousValue = 'undefined';
-      }
-
-      if (initialValue === 0) {
-        previousValue = 0;
-      }
-
-      for (let i = 0; i < this.length; i++) {
+      for (let i = 1; i < this.length; i++) {
         previousValue = callback(previousValue, this[i], i, this);
       }
 
       return previousValue;
     }
 
-    for (let i = 1; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       previousValue = callback(previousValue, this[i], i, this);
     }
 
