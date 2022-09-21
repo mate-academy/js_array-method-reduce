62c0d9fcc27b2f721b81d145054e7f172eae77a3
Artur Nagaycev <artur.nagaycev@gmail.com>
2022-09-21 14:14:26 +0300

    method reduce was implemented

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,23 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, startValue) {
+    let currentValue;
+    let firstIndex;
+
+    if (arguments.length < 2) {
+      currentValue = this[0];
+      firstIndex = 1;
+    } else {
+      currentValue = startValue;
+      firstIndex = 0;
+    }
+
+    for (let i = firstIndex; i < this.length; i++) {
+      currentValue = callback(currentValue, this[i], i, this);
+    }
+
+    return currentValue;
   };
 }
 
