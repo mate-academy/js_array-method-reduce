79c6319fba6270bccd6ecd9a8549fc065b963b0b
naveed-shoukat <shoukat.naveed@gmail.com>
2022-09-22 19:35:44 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,32 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum;
+    let startIndex;
+
+    // If initialvalue is NOT Provided
+    if (arguments.length === 1) {
+      sum = typeof this[0] === 'string' ? '' : 0;
+      startIndex = 0;
+    }
+
+    // If initialValue is explicitly provided as UNDEFINED
+    if (arguments.length > 1 && initialValue === undefined) {
+      sum = 'undefined';
+      startIndex = 0;
+    }
+
+    // If initialValue is Provided
+    if (arguments.length > 1 && initialValue !== undefined) {
+      sum = initialValue;
+      startIndex = 0;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum;
   };
 }
 
