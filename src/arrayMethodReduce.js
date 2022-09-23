da7087b896b31db815064f6ae824e234c9a620cf
Kolya Movchan <kmovchan261@gmail.com>
2022-09-23 20:39:48 +0300

    arrayMethodReduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let startValue = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length === 1) {
+      startValue = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      startValue = callback(startValue, this[i], i, this);
+    }
+
+    return startValue;
   };
 }
 
