5542bb8338d3423a9d19011e8b03844dc5017c6b
Viktoria Krasnyuk <krasnyuk_viktoria>
2022-09-26 19:38:50 +0300

    first commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prew = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      prew = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      prew = callback(prew, this[i], i, this);
+    }
+
+    return prew;
   };
 }
 
