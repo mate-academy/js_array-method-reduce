d1e9766020b7a709b7f9437beb0fef274aac7bba
Illia Kyselov <illya.kis99@gmail.com>
2022-10-07 20:13:53 +0300

    v3

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,6 +5,7 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
+<<<<<<< HEAD
     let amount = initialValue;
     let startIndex = 0;
 
@@ -12,6 +13,10 @@ function applyCustomReduce() {
       amount = this[0];
       startIndex = 1;
     }
+=======
+    let amount = (arguments.length < 2) ? this[0] : initialValue;
+    const startIndex = (arguments.length < 2) ? 1 : 0;
+>>>>>>> 49b63b6 (v3)
 
     for (let i = startIndex; i < this.length; i++) {
       amount = callback(amount, this[i], i, this);
