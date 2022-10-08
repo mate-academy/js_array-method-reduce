c27bddea77a389f518a7310c1d99482b0c7a3e2d
Vasyl Burban <lviv.vasil.st@gmail.com>
2022-10-08 10:16:34 +0300

    change

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,11 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
-    let startIndex = 0;
+    let prev;
+    let startIndex;
 
-    if (arguments.length < 2) {
-      prev = this[0];
-      startIndex = 1;
-    }
+    arguments.length < 2 ? prev = this[0] : prev = initialValue;
+    arguments.length < 2 ? startIndex = 1 : startIndex = 0;
 
     for (let i = startIndex; i < this.length; i++) {
       prev = callback(prev, this[i], i, this);
