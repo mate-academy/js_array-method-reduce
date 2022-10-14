78e8f4b8202024a33af4d5bab73110efde9019b1
metin <metinbicaksiz@gmail.com>
2022-10-14 22:12:50 +0300

    reduce applied

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,23 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    if (!this.length) {
+      return initialValue;
+    }
+
+    let acc = initialValue;
+    let index = 0;
+
+    if (arguments.length < 2) {
+      acc = this[0];
+      index = 1;
+    }
+
+    for (; index < this.length; index++) {
+      acc = callback(acc, this[index], index, this);
+    }
+
+    return acc;
   };
 }
 
