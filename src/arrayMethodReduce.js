cf375cd0368a118edc9b202bee816964dc5a07ed
Yevhen Petrovskyi <petrovskyi.yevhen@gmail.com>
2022-10-17 12:51:33 +0300

    Implement requested changes

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,9 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
-    let startIndex = 0;
-
-    if (arguments.length < 2) {
-      prev = this[0];
-      startIndex = 1;
-    }
+    const hasInitialValue = arguments.length < 2;
+    let prev = hasInitialValue ? this[0] : initialValue;
+    const startIndex = hasInitialValue ? 1 : 0;
 
     for (let i = startIndex; i < this.length; i++) {
       prev = callback(prev, this[i], i, this);
