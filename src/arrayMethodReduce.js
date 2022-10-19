d64ca1353539a153a4a7982f667da16815fac884
alexxxstep <astep2004@gmail.com>
2022-10-19 16:55:31 +0300

    Solution 2 - simplify code

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,37 +1,23 @@
-'use strict';
+"use strict";
 
 /**
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    if (this === null) {
-      throw new Error('Cant iterate over underfined or null');
-    }
-
+  [].__proto__.reduce2 = function (callback, initialValue) {
     let result;
     let i = 0;
-
-    const obj = Object(this);
-    const len = obj.length;
-
-    if (typeof callback !== 'function') {
-      throw new Error('callback is not a function');
-    }
 
     if (arguments.length >= 2) {
       result = initialValue;
     } else {
-      if (len === 0) {
-        throw new Error('reduce of empty array with no initial value');
-      }
-      result = obj[i];
+      result = this[i];
       i++;
     }
 
-    for (; i < obj.length; i++) {
-      if (i in obj) {
-        result = callback(result, obj[i], i, obj);
+    for (; i < this.length; i++) {
+      if (i in this) {
+        result = callback(result, this[i], i, this);
       }
     }
 
