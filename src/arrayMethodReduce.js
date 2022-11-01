cb3646b69986354779b676d2927de2b822fe9149
Yurii Ostapchuk <ostapchuk.y.o@gmail.com>
2022-11-01 17:11:19 +0200

    develop

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accum = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      accum = this[0];
+      startIndex = 1;
+    }
+
+    for (startIndex; startIndex < this.length; startIndex++) {
+      accum = callback(accum, this[startIndex], startIndex, this);
+    }
+
+    return accum;
   };
 }
 
