49e078452d4359f3c859cc6d6efc51389f672abc
Spirit <baolt.king@gmail.com>
2022-11-02 19:47:17 +0200

    complete

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,31 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(...args) {
+    const callback = args[0];
+    let initialValue = args[1];
+
+    if (args.length === 2 && initialValue === undefined) {
+      initialValue = 'undefined';
+    }
+
+    let startIndex;
+
+    let res;
+
+    if (initialValue !== undefined) {
+      startIndex = 0;
+      res = initialValue;
+    } else {
+      startIndex = 1;
+      res = this[0];
+    }
+
+    for (let x = startIndex; x < this.length; x++) {
+      res = callback(res, this[x], x, this);
+    }
+
+    return res;
   };
 }
 
