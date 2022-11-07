24889090f623e981c245714c9ed7a5cea9865349
Alex Tsimba <alexander.tsimba@gmail.com>
2022-11-07 23:12:03 +0200

    fixes after review

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,27 +1,23 @@
-"use strict";
+'use strict';
 
 /**
  * Implement method Reduce
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let total = initialValue;
+    let index = initialValue;
     let startIndex = 0;
 
-    if (!this.length && !initialValue) {
-      throw new TypeError('Empty array and no initialValue');
-    }
-
     if (arguments.length < 2) {
-      total = this[0];
+      index = this[0];
       startIndex = 1;
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      total = callback(total, this[i], i, this);
+      index = callback(index, this[i], i, this);
     }
 
-    return total;
+    return index;
   };
 }
 module.exports = applyCustomReduce;
