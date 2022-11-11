e304b5fbc88cd10c578742640a31a66b4c544f1b
Konstantin Burnazaki <kostas.burnazaki@gmail.com>
2022-11-11 11:18:43 +0200

    initial

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev;
+    let count = 0;
+
+    arguments.length === 2 ? prev = initialValue : prev = this[count++];
+
+    for (let i = count; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
