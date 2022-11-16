442fa8579bc342fa10d5598088988a23707e67a9
zbyn3k <coding707@mailbox.org>
2022-11-16 12:16:32 +0100

    Implemented custom Reduce method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,9 +3,22 @@
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prevElement = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      prevElement = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      prevElement = callback(prevElement, this[i], i, this);
+    }
+
+    return prevElement;
   };
 }
 
