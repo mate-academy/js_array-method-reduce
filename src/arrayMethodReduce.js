43c22a0faf6c6aa3aa2f3ae8e0c7f0fae3895974
Roman Bliashuk <romanblyashuk94@gmail.com>
2022-12-07 14:23:07 +0200

    Fixed comments

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,11 +5,13 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    const isInitialValue = (arguments.length === 2);
+    const hasInitialValue = (arguments.length === 2);
+    const startIndex = hasInitialValue ? 0 : 1;
+    let accumulator = hasInitialValue
+      ? initialValue
+      : this[0];
 
-    let accumulator = isInitialValue ? initialValue : this[0];
-
-    for (let i = isInitialValue ? 0 : 1; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
     }
 
