f04041eced62ed7c2992be6e5b07ff14dd5bd64c
mykolachupyak <chupyak@gmail.com>
2022-12-12 12:46:11 +0200

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let index = 0;
+    let accumulator = initialValue;
+
+    if (arguments.length === 1) {
+      accumulator = this[0];
+      index = 1;
+    }
+
+    for (let i = index; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
