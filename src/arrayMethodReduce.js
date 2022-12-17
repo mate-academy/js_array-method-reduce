16b958d99fc68e49e4db26ab5e4475a30ec804dd
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2022-12-17 19:35:42 +0200

    Used ternary operator

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,9 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let start = 0;
-    let previous = initialValue;
-
-    if (arguments.length < 2) {
-      previous = this[0];
-      start = 1;
-    }
+    const argsInArr = arguments.length > 1;
+    const start = argsInArr ? 0 : 1;
+    let previous = argsInArr ? initialValue : this[0];
 
     for (let i = start; i < this.length; i++) {
       previous = callback(previous, this[i], i, this);
