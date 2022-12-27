ca102330de2291b711e8d33856dea3f973968cc7
ihorivna007 <emmakorovnik@gmail.com>
2022-12-27 12:41:11 +0200

    initial commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(accumulator, startValue) {
+    let result = this[0];
+    let start = 1;
+
+    if (startValue === 0 || startValue) {
+      result = startValue;
+      start = 0;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      result = accumulator(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
