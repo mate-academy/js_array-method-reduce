c49dd3955594b84cf5497f807efb520c13b052fb
Roman Shevchenko <r.shevchenko87@gmail.com>
2022-12-30 18:30:33 +0200

    improvement

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -7,18 +7,18 @@ function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
     let start = 0;
-    let acc = initialValue;
+    let startIndex = initialValue;
 
     if (arguments.length === 1) {
       start = 1;
-      acc = this[0];
+      startIndex = this[0];
     }
 
     for (let i = start; i < this.length; i++) {
-      acc = callback(acc, this[i], i, this);
+      startIndex = callback(startIndex, this[i], i, this);
     }
 
-    return acc;
+    return startIndex;
   };
 }
 
