c780d89db3281b58c057341f455df6f620b5cd55
Vladyslav Ashurov <ashurovvladislav@gmail.com>
2023-01-03 00:03:27 +0200

    Changes after code review

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,8 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let result = initialValue;
-    let startIndex = 0;
-
-    if (arguments.length < 2) {
-      result = this[0];
-      startIndex = 1;
-    }
+    let result = (arguments.length < 2) ? this[0] : initialValue;
+    const startIndex = (arguments.length < 2) ? 1 : 0;
 
     for (let i = startIndex; i < this.length; i++) {
       result = callback(result, this[i], i, this);
