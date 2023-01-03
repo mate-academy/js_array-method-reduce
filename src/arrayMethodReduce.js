8d0175eaca314f9018c9d8c522b414868c4b162a
haduigon <haduigon@gmail.com>
2023-01-03 18:22:52 +0200

    using ternary operator

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,8 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let start = initialValue;
-    let startIndex = 0;
-
-    if (arguments.length < 2) {
-      start = this[0];
-      startIndex = 1;
-    }
+    let start = arguments.length < 2 ? this[0] : initialValue;
+    const startIndex = arguments.length < 2 ? 1 : 0;
 
     for (let i = startIndex; i < this.length; i++) {
       start = callback(start, this[i], i, this);
