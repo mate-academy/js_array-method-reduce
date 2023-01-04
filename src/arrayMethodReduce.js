30c08f087702f71c674266da4593377fcb40a36b
Sviatoslav <sv_dubov@ukr.net>
2023-01-05 01:32:41 +0200

    Rename prev

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
+    let previous = initialValue;
     let startIndex = 0;
 
     if (arguments.length < 2) {
-      prev = this[0];
+      previous = this[0];
       startIndex = 1;
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      prev = callback(prev, this[i], i, this);
+      previous = callback(previous, this[i], i, this);
     }
 
-    return prev;
+    return previous;
   };
 }
 
