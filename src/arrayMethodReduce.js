ee148bbe0821fbb84acf185641b308d3d6a08a33
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-01-05 01:22:44 +0200

    minor refactor

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,14 +5,15 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    const initPassed = arguments.length === 2;
+    const initPassed = arguments.length >= 2;
 
     if (!initPassed && !this.length) {
       throw new TypeError('Reduce of empty array with no initial value');
     }
 
-    let accumulator = initPassed ? initialValue : this[0];
-    let index = initPassed ? 0 : 1;
+    let [accumulator, index] = initPassed
+      ? [initialValue, 0]
+      : [this[0], 1];
 
     while (index < this.length) {
       accumulator = callback(accumulator, this[index], index, this);
