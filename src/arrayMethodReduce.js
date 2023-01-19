735a6ada57d569b676cd6c710b59c75039e60b1a
Kate <kate@Kates-MacBook-Pro-16.local>
2023-01-19 18:55:11 +0100

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,9 +3,26 @@
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let acc = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length === 1) {
+      acc = this[0];
+      startIndex = 1;
+    } else if (arguments.length > 1 && initialValue === undefined) {
+      acc = 'undefined';
+    } else {
+      acc = initialValue;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      acc = callback(acc, this[i], i, this);
+    }
+
+    return acc;
   };
 }
 
