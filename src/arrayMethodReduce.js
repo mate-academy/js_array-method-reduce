491c635d5e2f1f2422276d431ab6369153af4145
Nazarii AlieKsieiev <nazarmetall@gmail.com>
2023-01-26 14:37:28 +0200

    Update-v2

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,18 +5,17 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
-    let startIndex = 0;
-
-    [prev, startIndex] = arguments.length < 2
+    const [prev, startIndex] = arguments.length < 2
       ? [this[0], 1]
-      : [prev, startIndex];
+      : [initialValue, 0];
+
+    let result = prev;
 
     for (let i = startIndex; i < this.length; i++) {
-      prev = callback(prev, this[i], i, this);
+      result = callback(result, this[i], i, this);
     }
 
-    return prev;
+    return result;
   };
 }
 
