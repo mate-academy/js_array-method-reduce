5e9914a0a59aed915ef17ff8e4491ffc157d560b
Denys <kolbasin1990@gmail.com>
2023-02-05 13:19:42 +0200

    js_array-method-reduce_v_03

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,9 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let i = arguments.length < 2 ? 1 : 0;
-    let result = arguments.length < 2 ? this[0] : initialValue;
+    const hasInitialValue = arguments.length < 2;
+    let i = hasInitialValue ? 1 : 0;
+    let result = hasInitialValue ? this[0] : initialValue;
 
     for (; i < this.length; i++) {
       result = callback(result, this[i], i, this);
