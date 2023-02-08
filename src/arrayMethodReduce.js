b49e2589a8ce4138d4b084e5c24614d48eddb18d
Єва <eva@Evas-MacBook-Air.local>
2023-02-08 09:34:11 +0200

    result

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let init = initialValue;
+    let start = 0;
+
+    if (arguments.length < 2) {
+      init = this[0];
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      init = callback(init, this[i], i, this);
+    }
+
+    return init;
   };
 }
 
