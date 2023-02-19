bc83bdd7e166e2a6d25e6f0a2145ab320dd8258c
Tolik Petras <toliketras4@gmail.com>
2023-02-19 19:24:41 +0200

    delete if block

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,8 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let startIndex = 0;
-    let newValue = initialValue;
-
-    if (arguments.length < 2) {
-      newValue = this[0];
-      startIndex = 1;
-    }
+    const startIndex = arguments.length === 1 ? 1 : 0;
+    let newValue = arguments.length === 1 ? this[0] : initialValue;
 
     for (let i = startIndex; i < this.length; i++) {
       newValue = callback(newValue, this[i], i, this);
