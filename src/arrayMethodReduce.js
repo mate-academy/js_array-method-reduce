edabfe8f34e239c7994e7dac90ee53bed0700e66
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-02-22 15:53:58 +0200

    Array Method Reduce - Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,20 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const size = arguments.length;
+    let acc = initialValue;
+    let from = 0;
+
+    if (size < 2) {
+      from = 1;
+      acc = this[0];
+    }
+
+    for (let i = from; i < this.length; i++) {
+      acc = callback(acc, this[i], i, this);
+    }
+
+    return acc;
   };
 }
 
