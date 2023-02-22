312e66ab93f412988f9050b3f8a03c12bfab2072
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-02-22 14:13:06 +0100

    create reduce method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator = arguments.length > 1 ? initialValue : this[0];
+    const firstElement = arguments.length > 1 ? 0 : 1;
+
+    for (let i = firstElement; i < this.length; i++) {
+      if (this[i] !== '') {
+        accumulator = callback(accumulator, this[i], i, this);
+      }
+    }
+
+    return accumulator;
   };
 }
 
