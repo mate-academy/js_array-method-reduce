c0cff05f779c3a34f3fc34551e426ab18096b077
Vadym Kolomiiets <maestrovadkol@gmail.com>
2023-02-23 14:47:34 +0200

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,17 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const argsLengthChecking = arguments.length < 2;
+
+    let accumulator = argsLengthChecking ? this[0] : initialValue;
+
+    const firstIndex = argsLengthChecking ? 1 : 0;
+
+    for (let i = firstIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
