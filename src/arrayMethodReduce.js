84004a8cfac321ed5183424d0b53746236c38e3b
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-02-24 14:33:33 +0200

    Array Method Reduce v.0.1 - Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,20 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = initialValue;
+    let fromIndex = 0;
+
+    if (arguments.length < 2) {
+      fromIndex = 1;
+      prev = this[0];
+    }
+
+    for (let i = fromIndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
-}
+};
 
 module.exports = applyCustomReduce;
