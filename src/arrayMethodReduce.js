9355de632588c70bbaf2ac46ac8aea1d1fb570e1
Oleksandr Nedobezhkin <nedobezhkin_ad@yahoo.com>
2023-02-25 11:23:08 +0200

    Array Method Reduce v.0.2 - if changed to ternary

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,15 +5,11 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let prev = initialValue;
-    let fromIndex = 0;
+    const withInitialValue = arguments.length > 1;
+    const startIndex = withInitialValue ? 0 : 1;
+    let prev = withInitialValue ? initialValue : this[0];
 
-    if (arguments.length < 2) {
-      fromIndex = 1;
-      prev = this[0];
-    }
-
-    for (let i = fromIndex; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       prev = callback(prev, this[i], i, this);
     }
 
