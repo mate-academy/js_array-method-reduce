cc66b06f62c86a444b4cfca091814dbc4a64572f
Yevhen Prymushko <113305104+yevhen1408@users.noreply.github.com>
2023-02-28 15:39:34 +0200

    Chenges made

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let previous = initialValue;
+    let accumulator = initialValue;
     let startIndex = 0;
 
     if (arguments.length === 1) {
-      previous = this[0];
+      accumulator = this[0];
       startIndex = 1;
     }
 
-    for (let i = startIndex; i < this.length; i++) {
-      previous = callback(previous, this[i], i, this);
+    for (let index = startIndex; index < this.length; index++) {
+      accumulator = callback(accumulator, this[index], index, this);
     }
 
-    return previous;
+    return accumulator;
   };
 }
 
