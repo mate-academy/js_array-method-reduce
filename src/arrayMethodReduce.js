e9a1e5a64cdeff66fcc4098c0c2aebf93f78f1b3
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-03-03 22:55:28 +0200

    custom reduce first try

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let startValue = initialValue;
+
+    if (arguments.length < 2) {
+      startValue = this[0];
+    }
+
+    const startIndex = (arguments.length < 2) ? 1 : 0;
+
+    for (let i = startIndex; i < this.length; i++) {
+      startValue = callback(startValue, this[i], i, this);
+    }
+
+    return startValue;
   };
 }
 
