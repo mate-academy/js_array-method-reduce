6bd65a4dcd3ecc3395f7a77919cf0da069174ce4
Olecsiy <infernoregent@gmail.com>
2023-03-05 14:46:50 +0200

    s1

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator = initialValue;
+    let initialIndex = 0;
+
+    if (arguments.length < 2) {
+      accumulator = this[0];
+      initialIndex = 1;
+    }
+
+    for (let i = initialIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
