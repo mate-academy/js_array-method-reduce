c1b2e9cadbe87816d4eeed0d50a809499a09447c
Vadym Kolomiiets <maestrovadkol@gmail.com>
2023-03-06 14:44:04 +0200

    Fixed

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,15 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    const noInitialValue = arguments.length < 2;
+    const noInitialValue = arguments.length === 1;
 
     let accumulator = noInitialValue
       ? this[0]
       : initialValue;
 
-    const firstIndex = noInitialValue ? 1 : 0;
+    const firstIndex = noInitialValue
+    ? 1
+    : 0;
 
     for (let i = firstIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
