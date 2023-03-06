1168e889561b5255b3c98a901f47fdd113c8347a
Maksym <maksymmaliuk98@gmail.com>
2023-03-06 12:24:15 +0200

    acc changed to  accumulator

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,19 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let acc = initialValue;
+    let accumulator = initialValue;
     let startIndex = 0;
 
     if (arguments.length < 2) {
-      acc = this[0];
+      accumulator = this[0];
       startIndex = 1;
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      acc = callback(acc, this[i], i, this);
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
-    return acc;
+    return accumulator;
   };
 }
 
