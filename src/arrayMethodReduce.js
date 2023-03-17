36e9837e03cd0103ac45be2f6d1c41bd76d3b6a0
Daria <hamdsad8@gmail.com>
2023-03-17 11:43:49 +0200

    created the reduce method

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,17 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(accamulatorFunc, initialValue) {
+    const hasInitialValue = arguments.length > 1;
+    let accamulator = hasInitialValue ? initialValue : this[0];
+
+    for (let i = hasInitialValue ? 0 : 1; i < this.length; i++) {
+      if (i in this) {
+        accamulator = accamulatorFunc(accamulator, this[i], i, this);
+      }
+    }
+
+    return accamulator;
   };
 }
 
