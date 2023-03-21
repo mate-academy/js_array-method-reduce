2d9faceba985660b448dcdaf7e224bb0ef5a09b7
Daria <hamdsad8@gmail.com>
2023-03-21 13:13:31 +0200

    fixed naming

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,17 +4,15 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(accamulatorFunc, initialValue) {
+  [].__proto__.reduce2 = function(accumulatorFunc, initialValue) {
     const hasInitialValue = arguments.length > 1;
-    let accamulator = hasInitialValue ? initialValue : this[0];
+    let accumulator = hasInitialValue ? initialValue : this[0];
 
     for (let i = hasInitialValue ? 0 : 1; i < this.length; i++) {
-      if (i in this) {
-        accamulator = accamulatorFunc(accamulator, this[i], i, this);
-      }
+      accumulator = accumulatorFunc(accumulator, this[i], i, this);
     }
 
-    return accamulator;
+    return accumulator;
   };
 }
 
