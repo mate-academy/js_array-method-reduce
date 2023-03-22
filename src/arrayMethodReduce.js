8be2da8d9317f8b1b3fdab021da1dc3a031ad561
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-03-22 23:43:34 +0200

    reduce solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,20 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const arr = this;
+    let accumulator = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length === 1) {
+      accumulator = arr[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < arr.length; i++) {
+      accumulator = callback(accumulator, arr[i], i, arr);
+    }
+
+    return accumulator;
   };
 }
 
