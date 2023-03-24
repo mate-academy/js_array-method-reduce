26bc50e6c49c5b924a57b581fdd3d61e14ca9cac
Oleksandr Bavdyk <sbavdik@gmail.com>
2023-03-24 16:21:52 +0200

    fixed comments

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,9 +6,10 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     const hasInitialValue = arguments.length > 1;
-    let sum = arguments.length > 1 ? initialValue : this[0];
+    const startIndex = hasInitialValue ? 0 : 1;
+    let sum = hasInitialValue ? initialValue : this[0];
 
-    for (let i = hasInitialValue ? 0 : 1; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       sum = callback(sum, this[i], i, this);
     }
 
