2aa45700129119734dcf25d3b803337fc6085c5c
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-03-25 17:07:44 +0200

    remove arr variable

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,17 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    const arr = this;
     let accumulator = initialValue;
     let startIndex = 0;
 
     if (arguments.length === 1) {
-      accumulator = arr[0];
+      accumulator = this[0];
       startIndex = 1;
     }
 
-    for (let i = startIndex; i < arr.length; i++) {
-      accumulator = callback(accumulator, arr[i], i, arr);
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
     return accumulator;
