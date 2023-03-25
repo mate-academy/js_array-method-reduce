587ebde9730b039002c5d8236fa22f462493411d
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-03-25 17:42:31 +0300

    task solved

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let prev = initialValue;
+    let n = 0;
+
+    if (arguments.length < 2) {
+      prev = this[0];
+      n = 1;
+    }
+
+    for (let i = n; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
