e3b26c39df200fcdd37fef974a46a9a5c63136a5
Zibi95 <sylwester.sob95@gmail.com>
2023-03-27 17:45:24 +0200

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,18 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const isTwoArguments = arguments.length === 2;
+
+    const startIndex = isTwoArguments ? 0 : 1;
+    let accumulator = isTwoArguments ? initialValue : this[0];
+
+    for (let i = startIndex; i < this.length; i++) {
+      const result = callback(accumulator, this[i], i, this);
+
+      accumulator = result;
+    }
+
+    return accumulator;
   };
 }
 
