0c6ac963766919a2ab28d96a4e8fa4ea866f55cb
Igor Galkin <igor_galkin@ukr.net>
2023-03-28 14:50:36 +0300

    1st commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, initialValue = 'undefined') {
+    let prevValue = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      prevValue = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      prevValue = callback(prevValue, this[i], i, this);
+    }
+
+    return prevValue;
   };
 }
 
