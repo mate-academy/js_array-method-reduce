d2fdb7b1963b6d434cbb8d79195d26498d84856a
victor buldenko <gookidoo@gmail.com>
2023-03-28 17:33:54 +0300

    done

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let start = this[0];
+    let i = 1;
+
+    if (arguments.length === 2) {
+      start = initialValue;
+      i = 0;
+    }
+
+    for (; i < this.length; i++) {
+      start = callback(start, this[i], i, this);
+    }
+
+    return start;
   };
 }
 
