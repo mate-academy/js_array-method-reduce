f5225da9a9f722496e9e9752bc27188bba0fde7b
Daniel <henio09@wp.pl>
2023-04-08 17:13:44 +0200

    simplification

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,21 +4,17 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(...args) {
-    const [callback, initialValue] = args;
-
+  [].__proto__.reduce2 = function(callback, initialValue) {
     let accumulator = initialValue;
     let index = 0;
 
-    if (args.length < 2) {
+    if (arguments.length < 2) {
       accumulator = this[0];
       index = 1;
     }
 
     while (index < this.length) {
-      const current = this[index];
-
-      accumulator = callback(accumulator, current, index, this);
+      accumulator = callback(accumulator, this[index], index, this);
       index++;
     }
 
