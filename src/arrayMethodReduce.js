d873eb0e55462e16b976a669d465b10d96da5831
Daniel <henio09@wp.pl>
2023-04-08 17:07:36 +0200

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,25 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(...args) {
+    const [callback, initialValue] = args;
+
+    let accumulator = initialValue;
+    let index = 0;
+
+    if (args.length < 2) {
+      accumulator = this[0];
+      index = 1;
+    }
+
+    while (index < this.length) {
+      const current = this[index];
+
+      accumulator = callback(accumulator, current, index, this);
+      index++;
+    }
+
+    return accumulator;
   };
 }
 
