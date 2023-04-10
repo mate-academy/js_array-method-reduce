499c11cb263d095dfe129471c3c4690fc4ce312c
Veronika Pastushkova <d9169393507@gmai.com>
2023-04-10 11:01:56 +0300

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,19 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let prev = initialValue;
+    let startİndex = 0;
+
+    if (arguments.length < 2) {
+      prev = this[0];
+      startİndex = 1;
+    }
+
+    for (let i = startİndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
