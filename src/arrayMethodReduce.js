e449aa0e9a47c0fc6c7ea14ab1e7c6f04ba81880
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-04-11 17:59:02 +0300

    second version

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,23 +5,9 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let start = 0;
-    let initValue = 0;
+    let accumulator = arguments.length > 1 ? initialValue : this[0];
 
-    if (arguments.length === 1) {
-      if (this.length === 0) {
-        throw new TypeError('Reduce of empty array with no initial value');
-      }
-
-      initValue = this[0];
-      start = 1;
-    } else if (arguments.length > 1) {
-      initValue = arguments[1];
-    }
-
-    let accumulator = initValue;
-
-    for (let i = start; i < this.length; i++) {
+    for (let i = accumulator === this[0] ? 1 : 0; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
     }
 
