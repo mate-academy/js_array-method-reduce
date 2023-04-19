ac43032b7ce8be1ce8ecf81322e9874366b40f03
DefaultGitUser <usingforgit@gmail.com>
2023-04-19 15:33:51 +0300

    sol

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let acc = initialValue;
+    let start = 0;
+
+    if (arguments.length < 2) {
+      acc = this[0];
+      start = 1;
+    }
+
+    for (let i = start; i < this.length; i++) {
+      acc = callback(acc, this[i], i, this);
+    }
+
+    return acc;
   };
 }
 
