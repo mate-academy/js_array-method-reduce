63897ce548574ef91597f28376af74e9241f8852
Roman <rdyakovich@gmail.com>
2023-04-24 18:07:52 +0300

    reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,20 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator = initialValue !== undefined ? initialValue : this[0];
+    const startingIndex = initialValue !== undefined ? 0 : 1;
+
+    for (let i = startingIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    if (initialValue !== undefined) {
+      accumulator = initialValue + accumulator;
+    } else if (typeof accumulator === 'string') {
+      accumulator = undefined + accumulator;
+    }
+
+    return accumulator;
   };
 }
 
