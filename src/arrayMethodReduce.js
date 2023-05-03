eb07fb0274b7760735349ec6e577a61618553721
domkab <dominykas.bobinas@gmail.com>
2023-05-03 10:45:27 +0300

    1.1.0 11 of 11 passed

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -2,8 +2,16 @@
 
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator = initialValue !== undefined ? initialValue : this[0];
-    const start = initialValue !== undefined ? 0 : 1;
+    let accumulator;
+    let start;
+
+    if (arguments.length >= 2) {
+      accumulator = initialValue;
+      start = 0;
+    } else {
+      accumulator = this[0];
+      start = 1;
+    }
 
     for (let i = start; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
