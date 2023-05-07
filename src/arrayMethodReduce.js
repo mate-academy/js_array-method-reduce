d87ad2fd0f6850c83bd736b28f4bc1b32a23e698
Hishchak Tetiana <tanaiosered@ukr.net>
2023-05-07 20:21:11 +0300

    SReduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let sum = initialValue;
+    let indexFirst = 0;
+
+    if (arguments.length < 2) {
+      sum = this[0];
+      indexFirst = 1;
+    }
+
+    for (let i = indexFirst; i < this.length; i++) {
+      sum = callback(sum, this[i], i, this);
+    }
+
+    return sum;
   };
 }
 
