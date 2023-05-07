cb7a770cebff05e7c1b8fe5dae655f33b19532a6
Vitalii Parkhomenko <poroh1991ukr@gmail.com>
2023-05-07 14:16:50 +0300

    fixes initialization index and acc

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -8,10 +8,17 @@ function applyCustomReduce() {
     let accumulator = initialValue;
     let initialIndex = 0;
 
-    if (arguments.length < 2 && this.length > 1) {
-      accumulator = this[0];
-      initialIndex++;
-    }
+    // if (arguments.length < 2 && this.length > 1) {
+    //   accumulator = this[0];
+    //   initialIndex = 1;
+    // }
+
+    accumulator
+      = (arguments.length < 2 && this.length > 1)
+        ? accumulator = this[0]
+        : initialValue;
+
+    initialIndex = !accumulator ? 0 : 1;
 
     for (let i = initialIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
