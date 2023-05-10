5dc8f998d5081b7f4386ec2a95d4b18e74fb1473
NazariiDmytryshyn <nazarter98@gmail.com>
2023-05-10 22:41:46 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,21 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.reduce2 = function(callback, initialValue) {
+    let prev = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      prev = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      prev = callback(prev, this[i], i, this);
+    }
+
+    return prev;
   };
 }
 
