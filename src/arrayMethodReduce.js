ded3e2b5ea6c14f2429ec6465e50aa4bebcb2175
Denys Didenko <ddidenko441@gmail.com>
2023-05-10 19:25:42 +0300

    rename start variable

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,14 +6,14 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     let accumulator = initialValue;
-    let start = 0;
+    let startIndex = 0;
 
     if (arguments.length < 2) {
       accumulator = this[0];
-      start = 1;
+      startIndex = 1;
     }
 
-    for (let i = start; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
     }
 
