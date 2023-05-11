1d6af863275154e4d311558671c4645f6daad702
Alla Nebozhenko <111634874+AllaNebozhenko@users.noreply.github.com>
2023-05-11 19:45:07 +0200

    Reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let index = 0;
+    let total = initialValue;
+
+    if (arguments.length === 1) {
+      index = 1;
+      total = this[0];
+    }
+
+    for (let i = index; i < this.length; i += 1) {
+      total = callback(total, this[i], i, this);
+    }
+
+    return total;
   };
 }
 
