dab1c8901ee6bffd01749fdc89fbb80983fccc97
Andrii <zisuan.od@gmail.com>
2023-05-11 11:18:22 +0300

    task solved

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -3,9 +3,25 @@
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator;
+    let startLoop;
+
+    if (arguments.length < 2) {
+      accumulator = this[0];
+      startLoop = 1;
+    } else {
+      accumulator = initialValue;
+      startLoop = 0;
+    }
+
+    for (let i = startLoop; this.length > i; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
