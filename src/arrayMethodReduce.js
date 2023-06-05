d816d9fc8b29bc9a9ed59ca16ca3cab026ad4132
Kinderwagen <83349437+Kinderwagen@users.noreply.github.com>
2023-06-05 14:30:37 +0200

    Update src/arrayMethodReduce.js
    Co-authored-by: Ivan Fesenko <61904995+IvanFesenko@users.noreply.github.com>

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -2,21 +2,17 @@
 
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator;
-
+    let accumulator = initialValue;
+    let index = 0
+    
     if (arguments.length < 2) {
       accumulator = this[0];
-
-      for (let i = 1; i < this.length; i++) {
+      index = 1;
+   }
+   
+    for (let i = index; i < this.length; i++) {
         accumulator = callback(accumulator, this[i], i, this);
       }
-    } else {
-      accumulator = initialValue;
-
-      for (let i = 0; i < this.length; i++) {
-        accumulator = callback(accumulator, this[i], i, this);
-      }
-    }
 
     return accumulator;
   };
