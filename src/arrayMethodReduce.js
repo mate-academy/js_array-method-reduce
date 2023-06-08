259fb1d0fccd27168cd2d2ad96f145a2ca601d7f
ihorivna007 <emmakorovnik@gmail.com>
2023-06-08 11:42:07 +0300

    added updates

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,13 +4,13 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(accumulator, ...initialValue) {
-    let result = this[0];
-    let start = 1;
+  [].__proto__.reduce2 = function(accumulator, initialValue) {
+    let result = initialValue;
+    let start = 0;
 
-    if (initialValue.length > 0) {
-      result = initialValue[0];
-      start = 0;
+    if (arguments.length < 2) {
+      result = this[0];
+      start = 1;
     }
 
     for (let i = start; i < this.length; i++) {
