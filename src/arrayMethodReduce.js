a7cae76e3624a8497b3c5447d9cf55cc6a208b6b
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-06-13 19:13:01 +0300

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result = initialValue !== undefined ? initialValue : this[0];
+    let loopFrom = initialValue !== undefined ? 0 : 1;
+
+    if (arguments.length === 2 && initialValue === undefined) {
+      result = initialValue;
+      loopFrom = 0;
+    }
+
+    for (let i = loopFrom; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
