d3abd3056b8e731a4a551b7bc7cced2c4f72d098
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-06-17 22:00:00 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, ...args) {
+    let finalValue = args[0] === undefined ? `${args[0]}` : args[0];
+    let i = 0;
+
+    if (args.length === 0) {
+      finalValue = this[0];
+      i = 1;
+    }
+
+    for (; i < this.length; i++) {
+      finalValue = callback(finalValue, this[i], i, this);
+    }
+
+    return finalValue;
   };
 }
 
