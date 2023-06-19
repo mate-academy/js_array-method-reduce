d7977b33a7397d538b464991f005934e7ca74360
artkas03 <zhonayer2017@gmail.com>
2023-06-19 15:46:31 +0300

    finished tasl

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,22 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const areBothArgumentPassed = arguments.length === 2;
+    let total = this[0];
+
+    if (typeof initialValue === 'number') {
+      total = initialValue;
+    } else if (areBothArgumentPassed) {
+      total = initialValue + '';
+    }
+
+    const startValue = areBothArgumentPassed ? 0 : 1;
+
+    for (let i = startValue; i < this.length; i++) {
+      total = callback(total, this[i], i, this);
+    }
+
+    return total;
   };
 }
 
