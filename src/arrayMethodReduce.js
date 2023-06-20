38de924c32cf301799fbc2c6551e6795b7e5776c
Diana <diana.gs202@gmail.com>
2023-06-20 12:17:41 +0300

    delete extra function

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,28 +5,15 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
+    let accumulator = arguments.length === 2 ? initialValue : this[0];
+    const startIndex = arguments.length === 2 ? 0 : 1;
 
-    let accumulator;
-
-    if (arguments.length === 2) {
-      accumulator = initialValue;
-
-      findResult(0, this);
-    } else {
-      accumulator = this[0];
-
-      findResult(1, this);
-    }
-
-    function findResult(start, input) {
-      for (let i = start; i < input.length; i++) {
-        accumulator = callback(accumulator, input[i], i, input);
-      }
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
     return accumulator;
   };
-
 }
 
 module.exports = applyCustomReduce;
