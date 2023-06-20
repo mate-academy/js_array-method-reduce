0e914bba1f7dd1aa5008381b6b8b6afc965e076b
artkas03 <zhonayer2017@gmail.com>
2023-06-20 12:18:18 +0300

    corrected style mistakes

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,18 +5,17 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    const areBothArgumentPassed = arguments.length === 2;
     let total = this[0];
 
     if (typeof initialValue === 'number') {
       total = initialValue;
-    } else if (areBothArgumentPassed) {
+    } else if (arguments.length > 1) {
       total = initialValue + '';
     }
 
-    const startValue = areBothArgumentPassed ? 0 : 1;
+    const startIndex = arguments.length > 1 ? 0 : 1;
 
-    for (let i = startValue; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       total = callback(total, this[i], i, this);
     }
 
