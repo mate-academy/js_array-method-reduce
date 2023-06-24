42cff27458036c7f46f648f2ff267563be28d6c9
Alina Isakova <adorosh062@gmail.com>
2023-06-24 21:20:02 +0300

    reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accum = arguments.length === 2 ? initialValue : this[0];
+    const start = arguments.length === 2 ? 0 : 1;
+
+    for (let i = start; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return accum;
   };
 }
 
