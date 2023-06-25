8682c7d5b2336f487385b7e16ce624f84cd43c58
Alina Isakova <adorosh062@gmail.com>
2023-06-25 12:56:51 +0300

    Solution with variable

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,12 +1,10 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accum = arguments.length === 2 ? initialValue : this[0];
-    const start = arguments.length === 2 ? 0 : 1;
+    const checkArgLength = arguments.length === 2;
+    let accum = checkArgLength ? initialValue : this[0];
+    const start = checkArgLength ? 0 : 1;
 
     for (let i = start; i < this.length; i++) {
       accum = callback(accum, this[i], i, this);
