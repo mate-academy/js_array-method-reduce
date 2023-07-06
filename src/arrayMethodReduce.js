2f645d48aa1738a61273887a72193df2e62490ed
Nikita Orliak <nikitonorl@gmail.com>
2023-07-06 19:01:39 +0200

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,23 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result;
+
+    if (!(arguments.length < 2)) {
+      result = initialValue;
+
+      for (let i = 0; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+      }
+    } else {
+      result = this[0];
+
+      for (let i = 1; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+      }
+    };
+
+    return result;
   };
 }
 
