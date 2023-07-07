346fd76480c699f1c93fb926b5b27a1fe37a9156
Luk2asz <133983506+Luk2asz@users.noreply.github.com>
2023-07-07 11:30:57 +0200

    Update arrayMethodReduce.js

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -12,12 +12,6 @@ function applyCustomReduce() {
     if (arguments.length === 1) {
       result = this[0];
       i++;
-
-      // for (let i = 1; i < this.length; i++) {
-      //   result = callback(result, this[i], i, this);
-      // }
-
-      // return result;
     }
 
     for (;i < this.length; i++) {
