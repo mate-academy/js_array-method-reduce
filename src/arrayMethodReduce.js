4cfbb3901815c1efd45aec4f7e4056a7212bfe3c
Andrii Solovei <solovey98.info@gmail.com>
2023-07-13 12:09:34 +0300

    implemented method reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let acum = initialValue;
+
+    if (arguments.length === 1 && typeof this[0] === 'number') {
+      acum = 0;
+    } else if (arguments.length === 1 && typeof this[0] === 'string') {
+      acum = '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      acum = callback(acum, this[i], i, this);
+    }
+
+    return acum;
   };
 }
 
