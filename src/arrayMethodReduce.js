da57ad4984015cb848ba6f23bb2c84f21add44cb
Volodymyr Alieksieiev <125214087+vovaAlieksieiev@users.noreply.github.com>
2023-07-22 13:46:17 +0200

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    const initualValuePassed = arguments.length > 1;
+
+    let result = initualValuePassed ? initialValue : this[0];
+    const start = initualValuePassed ? 0 : 1;
+
+    for (let i = start; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
