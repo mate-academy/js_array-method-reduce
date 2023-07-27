f92acba245f81e9fe3e6d3d26d924052bdf39c85
Volodymyr Alieksieiev <125214087+vovaAlieksieiev@users.noreply.github.com>
2023-07-27 11:48:38 +0200

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,14 +1,13 @@
 'use strict';
-
 /**
  * Implement method Reduce
  */
+
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    const initualValuePassed = arguments.length > 1;
-
-    let result = initualValuePassed ? initialValue : this[0];
-    const start = initualValuePassed ? 0 : 1;
+    const initialValuePassed = arguments.length > 1;
+    let result = initialValuePassed ? initialValue : this[0];
+    const start = initialValuePassed ? 0 : 1;
 
     for (let i = start; i < this.length; i++) {
       result = callback(result, this[i], i, this);
