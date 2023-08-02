8c801e478b0d540e1e14e9033ff43e61e5914c21
Kacper <leman_kacper@o2.pl>
2023-08-02 11:47:39 +0200

    soultion

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,21 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator, startIndex;
+
+    if (initialValue === undefined && arguments.length < 2) {
+      accumulator = this[0];
+      startIndex = 1;
+    } else {
+      accumulator = initialValue;
+      startIndex = initialValue === undefined ? 0 : 0;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
