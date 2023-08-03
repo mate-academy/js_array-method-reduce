3bfebcb51011bd003a66b7f24590e4d9025194b5
Anton Kuts <anton.a.kuts@gmail.com>
2023-08-03 07:51:57 +0300

    completed task 'method reduce'

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,8 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let acc = initialValue;
+    let i = 0;
+
+    if (arguments.length < 2) {
+      acc = this[i];
+      i++;
+    }
+
+    for (i; i < this.length; i++) {
+      acc = callback(acc, this[i], i, this);
+    }
+
+    return acc;
   };
 }
-
 module.exports = applyCustomReduce;
