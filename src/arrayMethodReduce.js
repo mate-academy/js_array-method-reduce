d8407167da7893974f000569115d366be2179f7f
Vitalii <fevitaliis@gmail.com>
2023-08-07 11:07:51 +0300

    change

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,22 +5,16 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator = initialValue;
+    let accumulator = arguments.length >= 2 ? initialValue : this[0];
+    const startIndex = arguments.length >= 2 ? 0 : 1;
 
-    if (arguments.length === 1) {
-      accumulator = this[0];
-
-      for (let i = 1; i < this.length; i++) {
-        accumulator = callback(accumulator, this[i], i, this);
-      }
-    } else {
-      for (let i = 0; i < this.length; i++) {
-        accumulator = callback(accumulator, this[i], i, this);
-      }
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
     return accumulator;
   };
   };
-
+  
+  
 module.exports = applyCustomReduce;
