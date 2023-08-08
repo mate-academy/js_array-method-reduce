b740447728849aa5b4f8851fbac06ac766071a64
Anton Tsurkan <wesses1999@gmail.com>
2023-08-08 14:37:29 +0300

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulator = initialValue;
+    let i = 0;
+
+    if (arguments.length === 1) {
+      accumulator = this[0];
+      i++;
+    }
+
+    for (; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
