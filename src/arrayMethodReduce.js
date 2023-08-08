28c6cbf51161b85700a25d4dc9f131caf093c7ed
Kacper <leman_kacper@o2.pl>
2023-08-08 12:40:03 +0200

    Improvment after review

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,14 +5,12 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator, startIndex;
+    let accumulator = initialValue;
+    let startIndex = 0;
 
-    if (initialValue === undefined && arguments.length < 2) {
-      accumulator = this[0];
+    if (arguments.length < 2) {
       startIndex = 1;
-    } else {
-      accumulator = initialValue;
-      startIndex = initialValue === undefined ? 0 : 0;
+      accumulator = this[0];
     }
 
     for (let i = startIndex; i < this.length; i++) {
