0905a543b5a6bc2da8445294831b0bbd99866d25
davinnchii <zaikoivan99@gmail.com>
2023-08-08 14:59:53 +0200

    Add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,9 +4,24 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, ...initialValue) {
+    let accumulator = initialValue.length > 0
+      ? initialValue[0]
+      : this[0];
+    const startIndex = initialValue.length ? 0 : 1;
+
+    if (startIndex === 1 && this.length > 0) {
+      accumulator = this[0];
+    } else if (startIndex === 0 && initialValue === undefined) {
+      accumulator = 'undefined';
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
-}
+};
 
 module.exports = applyCustomReduce;
