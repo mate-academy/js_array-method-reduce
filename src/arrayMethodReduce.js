69f3930e07db0db16e6bf3c98aebff2abb01daeb
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-08 23:08:03 +0300

    initial commit

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,20 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, props) {
+    let accum = props;
+    let count = 0;
+
+    if (arguments.length === 1) {
+      accum = this[0];
+      count++;
+    }
+
+    for (let i = count; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return accum;
   };
 }
 
