c14cabaeaf39caee2473c13b2c45460dae725fbd
Taras Voievoda <voevoda.taras@gmail.com>
2023-08-08 22:56:00 +0300

    change initializtion

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,13 +5,8 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let accumulator = initialValue;
-    let startIndex = 0;
-
-    if (arguments.length <= 1) {
-      accumulator = this[0];
-      startIndex = 1;
-    }
+    let accumulator = arguments.length <= 1 ? this[0] : initialValue;
+    const startIndex = arguments.length <= 1 ? 1 : 0;
 
     for (let i = startIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
