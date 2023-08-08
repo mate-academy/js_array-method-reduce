1887d2aa31ec6e9d8064067f40c357dfd960562c
Denys Poltorak <itwfrontend@gmail.com>
2023-08-08 16:43:11 +0300

    feat: add implementation of reduce

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,8 +4,22 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, ...initialValue) {
+    if (typeof callback !== 'function') {
+      throw new TypeError(
+        'missing argument 0 '
+        + 'when calling function Array.prototype.reduce'
+      );
+    }
+
+    let accumulator = initialValue.length ? initialValue[0] : this[0];
+    const start = initialValue.length ? 0 : 1;
+
+    for (let i = start; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
