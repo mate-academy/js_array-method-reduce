583ffd0b3a3dd330f3f1fb19759188a0fa0375e2
Olena Matsyshyna <helenmarth@Olenas-MacBook-Air.local>
2023-08-09 18:22:40 +0300

    solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result = initialValue;
+    let startedIndex = 0;
+
+    if (arguments.length < 2) {
+      result = this[0];
+      startedIndex = 1;
+    }
+
+    for (let i = startedIndex; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
