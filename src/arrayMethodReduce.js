6fa880da94983a434d3821c901fee61ec463cc9a
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-08-10 10:29:25 +0300

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,21 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let accumulator;
+
+    if (arguments.length === 2 && initialValue === undefined) {
+      accumulator = 'undefined' + this[0];
+    } else {
+      accumulator = initialValue === undefined ? this[0] : initialValue;
+    }
+
+    const currentIndex = initialValue === undefined ? 1 : 0;
+
+    for (let i = currentIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
