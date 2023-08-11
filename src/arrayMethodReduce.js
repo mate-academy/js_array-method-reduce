20d113da6d74ed319180655ea4c277081dd94ff9
KatOlista^^! <olya.katyushina@gmail.com>
2023-08-11 17:54:39 +0200

    add solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -1,11 +1,16 @@
 'use strict';
 
-/**
- * Implement method Reduce
- */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function(callback, ...initialValue) {
+    let [accum] = initialValue.length ? initialValue : [this[0]];
+
+    let i = accum === this[0] ? 1 : 0;
+
+    for (; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return accum;
   };
 }
 
