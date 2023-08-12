443cff18140d234feb556370bb426aabfb4240c7
Andrii Kuz <andrykuz4556@gmail.com>
2023-08-12 13:06:58 +0300

    Changes after review

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,9 +4,14 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, ...initialValue) {
-    const startIndex = initialValue.length ? 0 : 1;
-    let accumulator = initialValue.length ? initialValue[0] : this[0];
+  [].__proto__.reduce2 = function(callback, initialValue) {
+    let accumulator = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length === 1) {
+      accumulator = this[0];
+      startIndex++;
+    }
 
     for (let i = startIndex; i < this.length; i++) {
       accumulator = callback(accumulator, this[i], i, this);
