761723bceeb4620b9e717c5080c52661f121980e
Viktoria-Moroz <viktoriaandriivna25@gmail.com>
2023-08-13 16:48:48 +0300

    the task was complete

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,26 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result = '';
+
+    if (this.length === 0) {
+      return initialValue;
+    }
+
+    let startIndex = 1;
+    let accumulator = this[0];
+
+    if (arguments.length > 1) {
+      startIndex = 0;
+      accumulator = initialValue;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      result = callback(accumulator, this[i], i, this);
+      accumulator = result;
+    }
+
+    return result;
   };
 }
 
