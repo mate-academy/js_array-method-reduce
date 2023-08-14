5516115a53197fba09cfe1ffbe06880b0f0345b0
Ivan Oksom <oksom.ivan82@gmail.com>
2023-08-14 20:11:18 +0200

    Solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,30 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this.length === 0 && initialValue === undefined) {
+      throw new TypeError('Reduce of empty array with no initial value');
+    }
+
+    let accumulator;
+    let currentIndex;
+
+    if (arguments.length === 2) {
+      accumulator = initialValue;
+      currentIndex = 0;
+    } else {
+      accumulator = this[0];
+      currentIndex = 1;
+    }
+
+    for (; currentIndex < this.length; currentIndex++) {
+      accumulator = callback(
+        accumulator,
+        this[currentIndex],
+        currentIndex,
+        this);
+    }
+
+    return accumulator;
   };
 }
 
