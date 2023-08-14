c211e6744de01f1feb1a10acc9847489efa8d286
Viktoria-Moroz <viktoriaandriivna25@gmail.com>
2023-08-14 10:10:47 +0300

    fixed some issues

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,12 +5,6 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    let result = '';
-
-    if (this.length === 0) {
-      return initialValue;
-    }
-
     let startIndex = 1;
     let accumulator = this[0];
 
@@ -20,11 +14,10 @@ function applyCustomReduce() {
     }
 
     for (let i = startIndex; i < this.length; i++) {
-      result = callback(accumulator, this[i], i, this);
-      accumulator = result;
+      accumulator = callback(accumulator, this[i], i, this);
     }
 
-    return result;
+    return accumulator;
   };
 }
 
