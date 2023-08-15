9ef52231b10119d1803ab641c94c409d20922b2d
KatOlista^^! <olya.katyushina@gmail.com>
2023-08-15 21:00:10 +0200

    create startIndex variable

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,9 +4,9 @@ function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, ...initialValue) {
     let [accum] = initialValue.length ? initialValue : [this[0]];
 
-    let i = accum === this[0] ? 1 : 0;
+    const startIndex = accum === this[0] ? 1 : 0;
 
-    for (; i < this.length; i++) {
+    for (let i = startIndex; i < this.length; i++) {
       accum = callback(accum, this[i], i, this);
     }
 
