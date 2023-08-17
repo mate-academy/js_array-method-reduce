9c538da4c576af3b5f55e5936242bca0704d4f9d
Nazar Kyzenko <kyzenko1997@gmail.com>
2023-08-17 11:23:47 +0300

    TaskReduceDoneAttempOne

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -6,6 +6,14 @@
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
     // write code here
+    let accumulator = initialValue !== undefined ? initialValue : this[0];
+    const startIndex = initialValue !== undefined ? 0 : 1;
+  
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+  
+    return accumulator;
   };
 }
 
