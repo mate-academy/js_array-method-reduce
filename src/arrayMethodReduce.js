12285de72afbb0c859e343d68188386030145c09
Taras Niklevych <niklevychtaras@gmail.com>
2023-08-18 16:00:37 +0300

    Solution to task "Array method reduce"

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,14 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let result = arguments.length === 1 ? this[0] : initialValue;
+    const startIndex = arguments.length === 1 ? 1 : 0;
+
+    for (let i = startIndex; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
