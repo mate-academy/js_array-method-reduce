63021c28ac7b604e732bef7a81470b711cd3f731
Ekaterina Bessonnaya <iekatierinovna9090@gmail.com>
2023-08-19 15:56:32 +0300

    reduce method was empleimented

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,23 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (this.length === 0) {
+      return initialValue;
+    }
+
+    let start = 0;
+    let accum = initialValue;
+
+    if (arguments.length === 1) {
+      start = 1;
+      accum = this[0];
+    }
+
+    for (let i = start; i < this.length; i++) {
+      accum = callback(accum, this[i], i, this);
+    }
+
+    return accum;
   };
 }
 
