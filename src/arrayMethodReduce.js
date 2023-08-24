213857821f60d9f5d1328b8cb80edd613882ab61
Tommaso Giacopini <giacopini@bluewin.ch>
2023-08-24 16:14:29 +0200

    task solved

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,9 +4,49 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+  [].__proto__.reduce2 = function customReduce(callback, initialValue) {
+    if (this.length === 0 && initialValue === undefined) {
+      return undefined;
+    }
+
+    let accumulator = initialValue;
+    let startIndex = 0;
+
+    if (arguments.length < 2) {
+      accumulator = this[0];
+      startIndex = 1;
+    }
+
+    for (let i = startIndex; i < this.length; i++) {
+      accumulator = callback(accumulator, this[i], i, this);
+    }
+
+    return accumulator;
   };
 }
 
 module.exports = applyCustomReduce;
+
+/* {
+  if (this.length === 0 && initialValue === undefined) {
+    return undefined;
+  }
+
+  let accumulator;
+  let startIndex;
+
+  if (initialValue !== undefined) {
+    accumulator = initialValue;
+    startIndex = 0;
+  } else {
+    accumulator = this[0];
+    startIndex = 1;
+  }
+
+  for (let i = startIndex; i < this.length; i++) {
+    accumulator = callback(accumulator, this[i], i, this);
+  }
+
+  return accumulator;
+};
+} */
