e3f9aefdc32ad7a99162bfc91f1cdc7dd89e19ee
Danylo-Chumakov <danil26032003@gmail.com>
2023-08-24 14:35:51 +0300

    add task solution

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,22 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    if (arguments.length < 1) {
+      throw new TypeError('need at least one argument');
+    }
+
+    let result = initialValue !== undefined ? initialValue : this[0];
+    let i = initialValue !== undefined ? 0 : 1;
+
+    if (this.length === 0 && initialValue === undefined) {
+      return 'undefined' + this.join('');
+    }
+
+    for (; i < this.length; i++) {
+      result = callback(result, this[i], i, this);
+    }
+
+    return result;
   };
 }
 
