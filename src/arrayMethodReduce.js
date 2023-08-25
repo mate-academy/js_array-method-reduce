d724b03495c9099b74156ac55d01fd6340ea97a4
Tommaso Giacopini <giacopini@bluewin.ch>
2023-08-25 12:35:35 +0200

    mistakes corrected

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,7 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function customReduce(callback, initialValue) {
-    if (this.length === 0 && initialValue === undefined) {
+    if (!this.length && !initialValue) {
       return undefined;
     }
 
@@ -26,27 +26,3 @@ function applyCustomReduce() {
 }
 
 module.exports = applyCustomReduce;
-
-/* {
-  if (this.length === 0 && initialValue === undefined) {
-    return undefined;
-  }
-
-  let accumulator;
-  let startIndex;
-
-  if (initialValue !== undefined) {
-    accumulator = initialValue;
-    startIndex = 0;
-  } else {
-    accumulator = this[0];
-    startIndex = 1;
-  }
-
-  for (let i = startIndex; i < this.length; i++) {
-    accumulator = callback(accumulator, this[i], i, this);
-  }
-
-  return accumulator;
-};
-} */
