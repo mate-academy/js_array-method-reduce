6466788304370195f169e124afcaf699d18700ca
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2023-08-31 18:59:45 +0200

    remove comments

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -17,16 +17,3 @@ function applyCustomReduce() {
 }
 
 module.exports = applyCustomReduce;
-
-// function applyCustomReduce() {
-//   [].__proto__.reduce2 = function(callback, initialValue) {
-//     let accumulator = arguments.length <= 1 ? this[0] : initialValue;
-//     const startIndex = arguments.length <= 1 ? 1 : 0;
-
-//     for (let i = startIndex; i < this.length; i++) {
-//       accumulator = callback(accumulator, this[i], i, this);
-//     }
-
-//     return accumulator;
-//   };
-// }
