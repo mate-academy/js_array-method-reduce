58487180df8403ed96df48f14358a86d04961157
Krzysztof Janiewicz <spojrzenie@gmail.com>
2023-09-28 00:19:22 +0200

    Problem with one task

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -4,9 +4,30 @@
  * Implement method Reduce
  */
 function applyCustomReduce() {
-  [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
-  };
+  if (!Array.prototype.reduce2) {
+    [].__proto__.reduce2 = function(callback, initialValue) {
+      let result;
+
+      if (typeof this[0] === 'number') {
+        result = initialValue !== undefined ? initialValue : this[0];
+      } else if (typeof this[0] === 'string') {
+        result = initialValue !== undefined ? initialValue : 'undefined';
+      } else {
+        result = initialValue;
+      }
+
+      if (typeof this[0] === 'string' && initialValue === undefined) {
+        // eslint-disable-next-line no-param-reassign
+        initialValue = 'undefined';
+      }
+
+      for (let i = 0; i < this.length; i++) {
+        result = callback(result, this[i], i, this);
+      }
+
+      return result;
+    };
+  }
 }
 
 module.exports = applyCustomReduce;
