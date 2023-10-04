65430887fd4073eb6f4b0cc01397409236912d71
Damian <damian.adamczewski92@gmail.com>
2023-10-04 11:55:23 +0200

    make reduce methode

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let accumulate = arguments.length < 2
+      ? this[0]
+      : initialValue;
+
+    const startIndex = arguments.length < 2
+      ? 1
+      : 0;
+
+    for (let i = startIndex; i < this.length; i++) {
+      accumulate = callback(accumulate, this[i], i, this);
+    }
+
+    return accumulate;
   };
 }
 
