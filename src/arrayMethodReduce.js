3d8663034dbdbae89ff117b98dfff4d135f13700
Roman Sylman <woody.falcone.00@gmail.com>
2024-01-23 16:17:26 +0200

    develop

--- src/arrayMethodReduce.js
+++ src/arrayMethodReduce.js
@@ -5,7 +5,19 @@
  */
 function applyCustomReduce() {
   [].__proto__.reduce2 = function(callback, initialValue) {
-    // write code here
+    let initial = initialValue;
+    let startValue = 0;
+
+    if (arguments.length < 2) {
+      initial = this[0];
+      startValue = 1;
+    }
+
+    for (let i = startValue; i < this.length; i++) {
+      initial = callback(initial, this[i], i, this);
+    }
+
+    return initial;
   };
 }
 
